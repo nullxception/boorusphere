e5af780577219eea7fb0781b099d92c276d1be0b
Nauval Rizky <enuma.alrizky@gmail.com>
2021-04-26 07:01:33 +0700

    Redesign drawer

--- lib/views/components/home_drawer.dart
+++ lib/views/components/home_drawer.dart
@@ -9,40 +9,70 @@ class HomeDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          DrawerHeader(
-            padding: const EdgeInsets.all(18),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  alignment: Alignment.centerRight,
-                  child: _ThemeSwitcherButton(),
-                ),
-                const Text(
-                  'Boorusphere!',
-                  style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.w200,
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            physics: const BouncingScrollPhysics(),
+            child: ConstrainedBox(
+              constraints: constraints.copyWith(
+                minHeight: constraints.maxHeight,
+                maxHeight: double.infinity,
+              ),
+              child: IntrinsicHeight(
+                child: SafeArea(
+                  child: Flex(
+                    direction: Axis.vertical,
+                    children: [
+                      Container(
+                        alignment: Alignment.topLeft,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 16, vertical: 25),
+                              child: Text(
+                                'Boorusphere!',
+                                style: TextStyle(
+                                  fontSize: 28,
+                                  fontWeight: FontWeight.w200,
+                                ),
+                              ),
+                            ),
+                            const Divider(),
+                            _ServerSelection(),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          alignment: Alignment.bottomLeft,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              const Divider(),
+                              _ThemeSwitcherButton(),
+                              ListTile(
+                                title: const Text('Settings'),
+                                leading: const Icon(Icons.settings),
+                                dense: true,
+                                onTap: () => Navigator.pushNamed(
+                                    context, Routes.settings),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
+              ),
             ),
-            decoration: const BoxDecoration(color: Colors.blue),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 16, right: 16),
-            child: _ServerSelection(),
-          ),
-          const Divider(),
-          ListTile(
-            title: const Text('Settings'),
-            onTap: () => Navigator.pushNamed(context, Routes.settings),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
@@ -52,9 +82,11 @@ class _ThemeSwitcherButton extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final uiThemeHandler = useProvider(uiThemeProvider.notifier);
-    return IconButton(
-      icon: Icon(uiThemeHandler.icon),
-      onPressed: uiThemeHandler.cycle,
+    return ListTile(
+      title: const Text('Switch theme'),
+      leading: Icon(uiThemeHandler.icon),
+      dense: true,
+      onTap: uiThemeHandler.cycle,
     );
   }
 }
@@ -66,22 +98,40 @@ class _ServerSelection extends HookWidget {
     final activeServer = useProvider(activeServerProvider);
     final activeServerHandler = useProvider(activeServerProvider.notifier);
     final serverList = useProvider(serverListProvider);
+    final theme = Theme.of(context);
 
-    return DropdownButton(
-      underline: const SizedBox.shrink(),
-      isExpanded: true,
-      iconSize: 30.0,
-      value: activeServer.name,
-      items: serverList
-          .map((it) => DropdownMenuItem(value: it.name, child: Text(it.name)))
-          .toList(),
-      onChanged: (name) {
-        if (name is String) {
-          activeServerHandler.setActiveServer(name: name);
-          api.fetch(clear: true);
-          Navigator.pop(context);
-        }
-      },
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          child: Text('Servers', style: TextStyle(fontWeight: FontWeight.w600)),
+        ),
+        ...serverList
+            .map((e) => Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 0, horizontal: 8),
+                  child: ListTile(
+                    title: Text(e.name),
+                    leading: const Icon(Icons.public),
+                    dense: true,
+                    contentPadding:
+                        const EdgeInsets.symmetric(vertical: 0, horizontal: 8),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    selected: e.name == activeServer.name,
+                    selectedTileColor: theme.accentColor.withAlpha(
+                        theme.brightness == Brightness.light ? 50 : 25),
+                    onTap: () {
+                      activeServerHandler.setActiveServer(name: e.name);
+                      api.fetch(clear: true);
+                      Navigator.pop(context);
+                    },
+                  ),
+                ))
+            .toList(),
+      ],
     );
   }
 }
