9a01bdcaa9182e9666e238990ea6ce0dac7f7b81
Nauval Rizky <enuma.alrizky@gmail.com>
2021-07-12 01:02:19 +0700

    views: add simple favicon widget

---
+++ lib/views/components/favicon.dart
@@ -0,0 +1,27 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class Favicon extends StatelessWidget {
+  const Favicon({
+    Key? key,
+    required this.url,
+  }) : super(key: key);
+
+  final String url;
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl: url,
+      placeholder: (_, __) => const Icon(Icons.public),
+      errorWidget: (_, __, ___) => const Icon(Icons.public),
+      imageBuilder: (context, img) => Container(
+        width: IconTheme.of(context).size,
+        height: IconTheme.of(context).size,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          image: DecorationImage(image: img, fit: BoxFit.contain),
+        ),
+      ),
+    );
+  }
+}
