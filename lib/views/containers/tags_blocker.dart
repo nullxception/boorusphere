nullxception/boorusphere

commit 715163dea1b8bf99d779c72bd8196f2d86e8a1b1

treewide: Implement client-side tags blocker

lib/views/containers/tags_blocker.dart
@@ -1,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../provider/blocked_tags.dart';
+import '../../provider/common.dart';
+
+class TagsBlocker extends HookWidget {
+  void updateTags(BlockedTagsRepository repo, ValueNotifier storage) {
+    repo.mapped.then((it) {
+      storage.value = it;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final blockedTagsHandler = useProvider(blockedTagsProvider);
+    final blockedTags = useState({});
+
+    useEffect(() {
+      // Populate suggestion history on first build
+      updateTags(blockedTagsHandler, blockedTags);
+    }, [blockedTags]);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Blocked Tags'),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            blockedTags.value.isNotEmpty
+                ? ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: blockedTags.value.length,
+                    itemBuilder: (context, index) {
+                      final key = blockedTags.value.keys.elementAt(index);
+                      final tag = blockedTags.value.values.elementAt(index);
+                      return ListTile(
+                        title: Text(tag),
+                        leading: const Icon(Icons.tag),
+                        trailing: IconButton(
+                            onPressed: () {
+                              blockedTagsHandler.delete(key).then((value) {
+                                updateTags(blockedTagsHandler, blockedTags);
+                              });
+                            },
+                            icon: const Icon(Icons.close)),
+                      );
+                    },
+                  )
+                : const Center(
+                    child: Padding(
+                      padding: EdgeInsets.all(32),
+                      child: Text('No blocked tags yet'),
+                    ),
+                  ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            isScrollControlled: true,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(15),
+                topRight: Radius.circular(15),
+              ),
+            ),
+            builder: (context) {
+              return SingleChildScrollView(
+                child: Container(
+                  padding: MediaQuery.of(context).viewInsets,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const ListTile(
+                        contentPadding: EdgeInsets.fromLTRB(16, 16, 16, 0),
+                        title: Text(
+                          'Blocking a tags',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w100,
+                          ),
+                        ),
+                        subtitle: Padding(
+                          padding: EdgeInsets.only(top: 8.0),
+                          child: Text(
+                              'You can block multiple tags by separating it with space'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(16, 0, 16, 32),
+                        child: TextFormField(
+                          autofocus: true,
+                          onFieldSubmitted: (value) {
+                            final values = value.trim().split(' ');
+                            blockedTagsHandler.pushAll(values).then((value) {
+                              updateTags(blockedTagsHandler, blockedTags);
+                            });
+                          },
+                          decoration: const InputDecoration(
+                            border: UnderlineInputBorder(),
+                            labelText: 'Example: open_mouth explosion',
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}

lib/provider/api_provider.dart
@@ -59,8 +59,10 @@ class ApiProvider {
     }
   }
 
-  List<BooruPost> _parseHttpResponse(http.Response res) {
+  Future<List<BooruPost>> _parseHttpResponse(http.Response res) async {
     final searchTag = read(searchTagProvider);
+    final blockedTags = read(blockedTagsProvider);
+    final blocked = await blockedTags.listedEntries;
 
     if (res.statusCode != 200) {
       throw HttpException('Something went wrong [${res.statusCode}]');
@@ -97,15 +99,19 @@ class ApiProvider {
       final tags = _getEntry(post, '^(tags|tag_str)');
       final width = _parseJsonNumber(post, '^(image_wid|width)');
       final height = _parseJsonNumber(post, '^(image_hei|height)');
+      final tagList = tags.value.toString().trim().split(' ');
 
-      if (src != null && thumbnail != null && width > 0 && height > 0) {
+      final hasContent = width > 0 && height > 0;
+      final notBlocked = !tagList.any(blocked.contains);
+
+      if (src != null && thumbnail != null && hasContent && notBlocked) {
         result.add(
           BooruPost(
             id: id,
             src: src,
             displaySrc: displaySrc ?? src,
             thumbnail: thumbnail,
-            tags: tags.value.toString().trim().split(' '),
+            tags: tagList,
             width: width,
             height: height,
           ),
@@ -120,7 +126,8 @@ class ApiProvider {
     final activeServer = read(activeServerProvider);
     try {
       final res = await http.get(activeServer.composeSearchUrl(query));
-      return right(_parseHttpResponse(res));
+      final data = await _parseHttpResponse(res);
+      return right(data);
     } on Exception catch (e) {
       Fimber.d('Caught Exception', ex: e);
       return left(e);

lib/provider/blocked_tags.dart
@@ -1,0 +1,56 @@
+import 'package:fimber/fimber.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'common.dart';
+
+class BlockedTagsRepository {
+  final Reader read;
+
+  BlockedTagsRepository(this.read);
+
+  Future<Map> get mapped async {
+    final blocked = await read(blockedTagsBox);
+    return blocked.toMap();
+  }
+
+  Future<List<String>> get listedEntries async {
+    final blocked = await read(blockedTagsBox);
+    return blocked.values.map((it) => it.toString()).toList();
+  }
+
+  Future<void> delete(key) async {
+    final blocked = await read(blockedTagsBox);
+    blocked.delete(key);
+  }
+
+  Future<bool> checkExists({required String value}) async {
+    final tag = value.trim();
+    if (tag.isEmpty) throw Exception('value must not be empty');
+
+    final blocked = await read(blockedTagsBox);
+    if (blocked.isEmpty) return false;
+
+    return tag ==
+        blocked.values.firstWhere((it) => it == tag, orElse: () => '');
+  }
+
+  Future<void> push(String value) async {
+    final tag = value.trim();
+    if (tag.isEmpty) return;
+
+    final blocked = await read(blockedTagsBox);
+    try {
+      if (!await checkExists(value: tag)) {
+        blocked.add(tag);
+      }
+    } on Exception catch (e) {
+      Fimber.d('Caught an Exception', ex: e);
+    }
+  }
+
+  Future<void> pushAll(List<String> values) async {
+    for (var tag in values) {
+      await push(tag);
+    }
+  }
+}

lib/provider/common.dart
@@ -6,6 +6,7 @@ import '../model/booru_post.dart';
 import '../model/server_data.dart';
 import 'active_server.dart';
 import 'api_provider.dart';
+import 'blocked_tags.dart';
 import 'grid.dart';
 import 'safe_mode.dart';
 import 'search_history.dart';
@@ -19,10 +20,12 @@ import 'version.dart';
 final searchHistoryBox = Provider((_) => Hive.openBox('searchHistory'));
 final settingsBox = Provider((_) => Hive.openBox('settings'));
 final serversBox = Provider((_) => Hive.openBox('servers'));
+final blockedTagsBox = Provider((_) => Hive.openBox('blockedTags'));
 
 // Common Providers
 final searchHistoryProvider =
     Provider((ref) => SearchHistoryRepository(ref.read));
+final blockedTagsProvider = Provider((ref) => BlockedTagsRepository(ref.read));
 final pageLoadingProvider = StateProvider((_) => false);
 final errorMessageProvider = StateProvider((_) => '');
 final lastOpenedPostProvider = StateProvider((_) => -1);

lib/routes.dart
@@ -4,16 +4,19 @@ import 'views/containers/home.dart';
 import 'views/containers/licenses.dart';
 import 'views/containers/post.dart';
 import 'views/containers/settings.dart';
+import 'views/containers/tags_blocker.dart';
 
 class Routes {
   static const home = '';
   static const post = 'post';
+  static const tagsBlocker = 'tagsBlocker';
   static const settings = 'settings';
   static const licenses = 'licenses';
 
   static Map<String, WidgetBuilder> of(BuildContext context) => {
         home: (context) => Home(),
         post: (context) => Post(),
+        tagsBlocker: (context) => TagsBlocker(),
         settings: (context) => Settings(),
         licenses: (context) => Licenses(),
       };

lib/views/components/home_drawer.dart
@@ -56,6 +56,13 @@ class HomeDrawer extends StatelessWidget {
                             mainAxisSize: MainAxisSize.min,
                             children: [
                               const Divider(),
+                              ListTile(
+                                title: const Text('Blocked Tags'),
+                                leading: const Icon(Icons.block),
+                                dense: true,
+                                onTap: () => Navigator.pushNamed(
+                                    context, Routes.tagsBlocker),
+                              ),
                               _ThemeSwitcherButton(),
                               ListTile(
                                 title: const Text('Settings'),

commit 9ce6d4b12ae5cf52a164a99e641cd152562974c4

TagsBlocker: Fix blocked tag list cannot be scrolled (resolve #3)
Fixes: 715163dea1b8bf99d779c72bd8196f2d86e8a1b1

lib/views/containers/tags_blocker.dart
@@ -26,37 +26,36 @@ class TagsBlocker extends HookWidget {
       appBar: AppBar(
         title: const Text('Blocked Tags'),
       ),
-      body: Container(
-        child: Column(
-          children: [
-            blockedTags.value.isNotEmpty
-                ? ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: blockedTags.value.length,
-                    itemBuilder: (context, index) {
-                      final key = blockedTags.value.keys.elementAt(index);
-                      final tag = blockedTags.value.values.elementAt(index);
-                      return ListTile(
-                        title: Text(tag),
-                        leading: const Icon(Icons.tag),
-                        trailing: IconButton(
-                            onPressed: () {
-                              blockedTagsHandler.delete(key).then((value) {
-                                updateTags(blockedTagsHandler, blockedTags);
-                              });
-                            },
-                            icon: const Icon(Icons.close)),
-                      );
-                    },
-                  )
-                : const Center(
-                    child: Padding(
-                      padding: EdgeInsets.all(32),
-                      child: Text('No blocked tags yet'),
-                    ),
+      body: ListView(
+        children: [
+          blockedTags.value.isNotEmpty
+              ? ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: blockedTags.value.length,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    final key = blockedTags.value.keys.elementAt(index);
+                    final tag = blockedTags.value.values.elementAt(index);
+                    return ListTile(
+                      title: Text(tag),
+                      leading: const Icon(Icons.tag),
+                      trailing: IconButton(
+                          onPressed: () {
+                            blockedTagsHandler.delete(key).then((value) {
+                              updateTags(blockedTagsHandler, blockedTags);
+                            });
+                          },
+                          icon: const Icon(Icons.close)),
+                    );
+                  },
+                )
+              : const Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(32),
+                    child: Text('No blocked tags yet'),
                   ),
-          ],
-        ),
+                ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),