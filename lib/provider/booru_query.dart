nullxception/boorusphere

commit ea4c6c4ccf4d23e06333c0592bae866b483b99bd

treewide: Refactor providers
For more predictable behavior, migrate things to StateNotifierProvider
and use FutureProvider for the data init/restoration instead.
For Server data providers, initialize it from UI instead with the help of
useEffect() hook.

lib/provider/booru_query.dart
@@ -1,21 +1,19 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../model/server_query.dart';
-import 'hive_boxes.dart';
 import 'search_history.dart';
+import 'settings/safe_mode.dart';
+
+final booruQueryProvider =
+    StateNotifierProvider<BooruQueryNotifier, ServerQuery>((ref) {
+  final safeMode = ref.read(safeModeProvider);
+  return BooruQueryNotifier(ref.read, ServerQuery(safeMode: safeMode));
+});
 
 class BooruQueryNotifier extends StateNotifier<ServerQuery> {
-  BooruQueryNotifier(this.read) : super(const ServerQuery()) {
-    _init();
-  }
+  BooruQueryNotifier(this.read, ServerQuery initState) : super(initState);
 
   final Reader read;
-
-  void _init() async {
-    final prefs = await read(settingsBox);
-    final enabled = prefs.get('server_safe_mode') ?? true;
-    state = state.copyWith(safeMode: enabled);
-  }
 
   Future<void> setTag({required String query}) async {
     final searchHistory = read(searchHistoryProvider);
@@ -28,12 +26,8 @@ class BooruQueryNotifier extends StateNotifier<ServerQuery> {
   Future<void> setSafeMode(enabled) async {
     if (state.safeMode != enabled) {
       state = state.copyWith(safeMode: enabled);
-      final prefs = await read(settingsBox);
-      prefs.put('server_safe_mode', enabled);
+      final safeModeNotifier = read(safeModeProvider.notifier);
+      safeModeNotifier.enable(enabled);
     }
   }
 }
-
-final booruQueryProvider =
-    StateNotifierProvider<BooruQueryNotifier, ServerQuery>(
-        (ref) => BooruQueryNotifier(ref.read));

lib/provider/settings/grid.dart
@@ -1,0 +1,32 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../hive_boxes.dart';
+
+final _savedGrid =
+    FutureProvider<int>((ref) async => await GridState.restore(ref));
+
+final gridProvider = StateNotifierProvider<GridState, int>((ref) {
+  final fromSettings =
+      ref.read(_savedGrid).maybeWhen(data: (data) => data, orElse: () => 0);
+
+  return GridState(ref.read, fromSettings);
+});
+
+class GridState extends StateNotifier<int> {
+  GridState(this.read, int initState) : super(initState);
+
+  final Reader read;
+
+  Future<void> rotate() async {
+    state = state < 2 ? state + 1 : 0;
+    final settings = await read(settingsBox);
+    settings.put(boxKey, state);
+  }
+
+  static Future<int> restore(FutureProviderRef ref) async {
+    final settings = await ref.read(settingsBox);
+    return settings.get(GridState.boxKey) ?? 0;
+  }
+
+  static const boxKey = 'timeline_grid_number';
+}

lib/provider/settings/safe_mode.dart
@@ -1,0 +1,33 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../hive_boxes.dart';
+
+final _savedSafeMode =
+    FutureProvider<bool>((ref) async => await SafeModeState.restore(ref));
+
+final safeModeProvider = StateNotifierProvider<SafeModeState, bool>((ref) {
+  final fromSettings = ref
+      .read(_savedSafeMode)
+      .maybeWhen(data: (data) => data, orElse: () => true);
+
+  return SafeModeState(ref.read, fromSettings);
+});
+
+class SafeModeState extends StateNotifier<bool> {
+  SafeModeState(this.read, initData) : super(initData);
+
+  final Reader read;
+
+  Future<void> enable(bool value) async {
+    state = value;
+    final settings = await read(settingsBox);
+    settings.put(boxKey, value);
+  }
+
+  static Future<bool> restore(FutureProviderRef ref) async {
+    final settings = await ref.read(settingsBox);
+    return settings.get(SafeModeState.boxKey) ?? true;
+  }
+
+  static const boxKey = 'server_safe_mode';
+}

lib/provider/settings/theme.dart
@@ -1,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../hive_boxes.dart';
+
+final _savedThemeMode =
+    FutureProvider<ThemeMode>((ref) async => await ThemeModeState.restore(ref));
+
+final themeModeProvider =
+    StateNotifierProvider<ThemeModeState, ThemeMode>((ref) {
+  final fromSettings = ref
+      .read(_savedThemeMode)
+      .maybeWhen(data: (data) => data, orElse: () => ThemeMode.system);
+
+  return ThemeModeState(ref.read, fromSettings);
+});
+
+final _savedDarkerTheme =
+    FutureProvider<bool>((ref) async => await DarkerThemeState.restore(ref));
+
+final darkerThemeProvider =
+    StateNotifierProvider<DarkerThemeState, bool>((ref) {
+  final fromSettings = ref
+      .read(_savedDarkerTheme)
+      .maybeWhen(data: (data) => data, orElse: () => false);
+
+  return DarkerThemeState(ref.read, fromSettings);
+});
+
+class ThemeModeState extends StateNotifier<ThemeMode> {
+  ThemeModeState(this.read, ThemeMode initState) : super(initState);
+
+  final Reader read;
+
+  Future<void> restoreFromPreference() async {
+    final settings = await read(settingsBox);
+    final modeValue = settings.get(boxKey) ?? ThemeMode.system.index;
+    state = ThemeMode.values[modeValue];
+  }
+
+  Future<void> setMode({required ThemeMode mode}) async {
+    state = mode;
+    final settings = await read(settingsBox);
+    settings.put(boxKey, mode.index);
+  }
+
+  void cycleTheme() async {
+    switch (state) {
+      case ThemeMode.dark:
+        setMode(mode: ThemeMode.light);
+        break;
+      case ThemeMode.light:
+        setMode(mode: ThemeMode.system);
+        break;
+      default:
+        setMode(mode: ThemeMode.dark);
+        break;
+    }
+  }
+
+  static Future<ThemeMode> restore(FutureProviderRef<ThemeMode> ref) async {
+    final settings = await ref.read(settingsBox);
+    return settings.get(ThemeModeState.boxKey) ?? ThemeMode.system;
+  }
+
+  static const boxKey = 'theme_mode';
+}
+
+class DarkerThemeState extends StateNotifier<bool> {
+  DarkerThemeState(this.read, bool initState) : super(initState);
+
+  final Reader read;
+
+  void enable(bool value) async {
+    state = value;
+    final prefs = await read(settingsBox);
+    prefs.put(boxKey, value);
+  }
+
+  static const boxKey = 'ui_theme_darker';
+
+  static Future<bool> restore(FutureProviderRef<bool> ref) async {
+    final settings = await ref.read(settingsBox);
+    return settings.get(ThemeModeState.boxKey) ?? false;
+  }
+}

lib/provider/settings/video_player.dart
@@ -1,0 +1,40 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../hive_boxes.dart';
+
+final _savedVideoPlayerMute = FutureProvider<bool>(
+    (ref) async => await VideoPlayerMuteState.restore(ref));
+
+final videoPlayerMuteProvider =
+    StateNotifierProvider<VideoPlayerMuteState, bool>((ref) {
+  final fromSettings = ref
+      .read(_savedVideoPlayerMute)
+      .maybeWhen(data: (data) => data, orElse: () => false);
+
+  return VideoPlayerMuteState(ref.read, fromSettings);
+});
+
+class VideoPlayerMuteState extends StateNotifier<bool> {
+  VideoPlayerMuteState(this.read, bool initState) : super(initState);
+
+  final Reader read;
+
+  Future<void> restoreFromPreference() async {
+    final prefs = await read(settingsBox);
+    state = prefs.get(boxKey) ?? false;
+  }
+
+  Future<void> toggle() async {
+    final newState = !state;
+    state = newState;
+    final settings = await read(settingsBox);
+    settings.put(boxKey, newState);
+  }
+
+  static const boxKey = 'videoplayer_mute';
+
+  static Future<bool> restore(FutureProviderRef<bool> ref) async {
+    final settings = await ref.read(settingsBox);
+    return settings.get(VideoPlayerMuteState.boxKey) ?? false;
+  }
+}

lib/provider/grid.dart
@@ -1,26 +1,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'hive_boxes.dart';
-
-class GridNotifier extends StateNotifier<int> {
-  GridNotifier(this.read) : super(0) {
-    restoreFromPreference();
-  }
-
-  static const gridKey = 'timeline_grid_number';
-  final Reader read;
-
-  Future<void> restoreFromPreference() async {
-    final prefs = await read(settingsBox);
-    state = prefs.get(gridKey) ?? 0;
-  }
-
-  Future<void> rotate() async {
-    state = state < 2 ? state + 1 : 0;
-    final prefs = await read(settingsBox);
-    prefs.put(gridKey, state);
-  }
-}
-
-final gridProvider =
-    StateNotifierProvider<GridNotifier, int>((ref) => GridNotifier(ref.read));

lib/provider/video_player.dart
@@ -1,32 +1,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'hive_boxes.dart';
-
-class VideoPlayerNotifier extends ChangeNotifier {
-  static const keyMute = 'videoplayer_mute';
-
-  final Reader read;
-
-  bool _mute = false;
-
-  VideoPlayerNotifier(this.read) {
-    _init();
-  }
-
-  Future<void> _init() async {
-    final prefs = await read(settingsBox);
-    _mute = prefs.get(keyMute) ?? false;
-  }
-
-  get mute => _mute;
-
-  set mute(value) {
-    _mute = value;
-    read(settingsBox).then((it) => it.put(keyMute, value));
-    notifyListeners();
-  }
-}
-
-final videoPlayerProvider =
-    ChangeNotifierProvider((ref) => VideoPlayerNotifier(ref.read));

lib/main.dart
@@ -9,20 +9,37 @@ import 'model/booru_post.dart';
 import 'model/download_entry.dart';
 import 'model/search_history.dart';
 import 'model/server_data.dart';
-import 'provider/app_theme.dart';
+import 'provider/booru_api.dart';
 import 'provider/downloader.dart';
+import 'provider/server_data.dart';
+import 'provider/settings/active_server.dart';
+import 'provider/settings/theme.dart';
 import 'routes.dart';
+import 'util/app_theme.dart';
 import 'views/components/bouncing_scroll.dart';
 
 class Boorusphere extends HookConsumerWidget {
   const Boorusphere({super.key});
 
+  Future<void> initServerData(WidgetRef ref) async {
+    final api = ref.read(booruApiProvider);
+    final serverDataNotifier = ref.read(serverDataProvider.notifier);
+    final activeServerNotifier = ref.read(activeServerProvider.notifier);
+
+    await serverDataNotifier.populateData();
+    await activeServerNotifier.restoreFromPreference();
+    api.posts.clear();
+    api.fetch();
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final appTheme = ref.watch(appThemeProvider);
+    final themeMode = ref.watch(themeModeProvider);
+    final isDarkerTheme = ref.watch(darkerThemeProvider);
     final downloadNotifier = ref.watch(downloadProvider.notifier);
 
     useEffect(() {
+      initServerData(ref);
       downloadNotifier.register();
       return () {
         downloadNotifier.unregister();
@@ -33,9 +50,10 @@ class Boorusphere extends HookConsumerWidget {
       builder: (ColorScheme? maybeLight, ColorScheme? maybeDark) => MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Boorusphere',
-        theme: appTheme.schemeFrom(maybeLight, Brightness.light),
-        darkTheme: appTheme.schemeFrom(maybeDark, Brightness.dark),
-        themeMode: appTheme.current,
+        theme: AppTheme.schemeFrom(maybeLight, AppThemeVariant.light),
+        darkTheme: AppTheme.schemeFrom(maybeDark,
+            isDarkerTheme ? AppThemeVariant.darker : AppThemeVariant.dark),
+        themeMode: themeMode,
         initialRoute: Routes.home,
         routes: Routes.of(context),
         builder: (context, widget) => ScrollConfiguration(

commit 5dd1dacd41e0ff1fa84f95ba1fb3b1d001c6b3f0

providers: use watch() on settings restoration
idk why but we'll never get the result properly if using read()
inside StateNotifierProvider.

Fixes: ea4c6c4cc ("treewide: Refactor providers")

lib/provider/booru_query.dart
@@ -6,7 +6,7 @@ import 'settings/safe_mode.dart';
 
 final booruQueryProvider =
     StateNotifierProvider<BooruQueryNotifier, ServerQuery>((ref) {
-  final safeMode = ref.read(safeModeProvider);
+  final safeMode = ref.watch(safeModeProvider);
   return BooruQueryNotifier(ref.read, ServerQuery(safeMode: safeMode));
 });
 

lib/provider/settings/blur_explicit_post.dart
@@ -8,7 +8,7 @@ final _savedBlurExplicitPost = FutureProvider<bool>(
 final blurExplicitPostProvider =
     StateNotifierProvider<BlurExplicitPostState, bool>((ref) {
   final fromSettings = ref
-      .read(_savedBlurExplicitPost)
+      .watch(_savedBlurExplicitPost)
       .maybeWhen(data: (data) => data, orElse: () => true);
 
   return BlurExplicitPostState(ref.read, fromSettings);

lib/provider/settings/grid.dart
@@ -7,7 +7,7 @@ final _savedGrid =
 
 final gridProvider = StateNotifierProvider<GridState, int>((ref) {
   final fromSettings =
-      ref.read(_savedGrid).maybeWhen(data: (data) => data, orElse: () => 0);
+      ref.watch(_savedGrid).maybeWhen(data: (data) => data, orElse: () => 0);
 
   return GridState(ref.read, fromSettings);
 });

lib/provider/settings/safe_mode.dart
@@ -7,7 +7,7 @@ final _savedSafeMode =
 
 final safeModeProvider = StateNotifierProvider<SafeModeState, bool>((ref) {
   final fromSettings = ref
-      .read(_savedSafeMode)
+      .watch(_savedSafeMode)
       .maybeWhen(data: (data) => data, orElse: () => true);
 
   return SafeModeState(ref.read, fromSettings);

lib/provider/settings/theme.dart
@@ -9,7 +9,7 @@ final _savedThemeMode =
 final themeModeProvider =
     StateNotifierProvider<ThemeModeState, ThemeMode>((ref) {
   final fromSettings = ref
-      .read(_savedThemeMode)
+      .watch(_savedThemeMode)
       .maybeWhen(data: (data) => data, orElse: () => ThemeMode.system);
 
   return ThemeModeState(ref.read, fromSettings);
@@ -21,7 +21,7 @@ final _savedDarkerTheme =
 final darkerThemeProvider =
     StateNotifierProvider<DarkerThemeState, bool>((ref) {
   final fromSettings = ref
-      .read(_savedDarkerTheme)
+      .watch(_savedDarkerTheme)
       .maybeWhen(data: (data) => data, orElse: () => false);
 
   return DarkerThemeState(ref.read, fromSettings);

lib/provider/settings/video_player.dart
@@ -8,7 +8,7 @@ final _savedVideoPlayerMute = FutureProvider<bool>(
 final videoPlayerMuteProvider =
     StateNotifierProvider<VideoPlayerMuteState, bool>((ref) {
   final fromSettings = ref
-      .read(_savedVideoPlayerMute)
+      .watch(_savedVideoPlayerMute)
       .maybeWhen(data: (data) => data, orElse: () => false);
 
   return VideoPlayerMuteState(ref.read, fromSettings);