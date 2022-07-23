nullxception/boorusphere

commit 2c6ea44a6e08eea12d84f484387fc8d73cb68ab7

Downloader: allow download on other url

lib/provider/downloader.dart
@@ -159,7 +159,7 @@ class Downloader extends ChangeNotifier {
         .lastWhere((it) => it.contains(RegExp(r'.+\..+')));
   }
 
-  Future<void> download(BooruPost post) async {
+  Future<void> download(BooruPost post, {String? url}) async {
     final downloadPath = await platformDownloadPath;
 
     if (!await _isDirWritable(downloadPath)) {
@@ -173,7 +173,7 @@ class Downloader extends ChangeNotifier {
     }
 
     final taskId = await FlutterDownloader.enqueue(
-        url: post.src,
+        url: url ?? post.src,
         savedDir: booruDir.absolute.path,
         showNotification: true,
         openFileFromNotification: true);

commit aa1812d39b029cfd2939f28ff9968a3e84daa59c

Downloader: Fix incorrect file name when downloading sample file
Fixes: 2c6ea44a6 ("Downloader: allow download on other url")

lib/provider/downloader.dart
@@ -160,6 +160,7 @@ class Downloader extends ChangeNotifier {
   }
 
   Future<void> download(BooruPost post, {String? url}) async {
+    final fileUrl = url ?? post.originalFile;
     final downloadPath = await platformDownloadPath;
 
     if (!await _isDirWritable(downloadPath)) {
@@ -173,14 +174,14 @@ class Downloader extends ChangeNotifier {
     }
 
     final taskId = await FlutterDownloader.enqueue(
-        url: url ?? post.originalFile,
+        url: fileUrl,
         savedDir: booruDir.absolute.path,
         showNotification: true,
         openFileFromNotification: true);
 
     if (taskId != null) {
       final destination =
-          '${booruDir.absolute.path}/${getFileNameFromUrl(post.originalFile)}';
+          '${booruDir.absolute.path}/${getFileNameFromUrl(fileUrl)}';
       final entry =
           DownloadEntry(id: taskId, booru: post, destination: destination);
       _addEntry(entry: entry);

lib/views/containers/downloads.dart
@@ -51,8 +51,8 @@ class DownloadsPage extends HookConsumerWidget {
                 ),
               ),
             ...downloader.entries.map((it) {
-              final fileName = Uri.decodeFull(
-                  downloader.getFileNameFromUrl(it.booru.originalFile));
+              final fileName =
+                  Uri.decodeFull(downloader.getFileNameFromUrl(it.destination));
               final progress = downloader.getProgress(it.id);
 
               return ListTile(