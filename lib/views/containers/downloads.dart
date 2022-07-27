nullxception/boorusphere

commit 71849fea1137a511bc452fb50586ee029696727e

DownloadsPage: Improve entry description layout
* use small circular indicator to indicate download progress
* use icons to indicate other status (completed/failed/canceled)

Issues: #13

lib/views/containers/downloads.dart
@@ -1,8 +1,10 @@
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:separated_row/separated_row.dart';
 
 import '../../model/download_entry.dart';
+import '../../model/download_status.dart';
 import '../../provider/downloader.dart';
 import '../../provider/settings/downloads/group_by_server.dart';
 import '../components/expandable_group_list_view.dart';
@@ -156,6 +158,32 @@ class _DownloadEntryView extends ConsumerWidget {
 
   final DownloadEntry entry;
 
+  IconData downloadStatusIconOf(DownloadStatus status) {
+    switch (status) {
+      case DownloadStatus.downloaded:
+        return Icons.download_done_rounded;
+      case DownloadStatus.downloading:
+        return Icons.downloading_rounded;
+      case DownloadStatus.canceled:
+      case DownloadStatus.failed:
+        return Icons.cancel_rounded;
+      default:
+        return Icons.file_open;
+    }
+  }
+
+  Color downloadStatusColorOf(DownloadStatus status, ColorScheme scheme) {
+    switch (status) {
+      case DownloadStatus.downloaded:
+        return Colors.lightBlueAccent;
+      case DownloadStatus.canceled:
+      case DownloadStatus.failed:
+        return Colors.pinkAccent;
+      default:
+        return scheme.onSurface;
+    }
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final downloader = ref.watch(downloadProvider);
@@ -170,13 +198,34 @@ class _DownloadEntryView extends ConsumerWidget {
       ),
       subtitle: Padding(
         padding: const EdgeInsets.only(top: 8),
-        child: Text(
-          [
-            if (progress.status.isDownloading) '${progress.progress}%',
-            progress.status.name,
-            if (!groupByServer) '• ${entry.post.serverName}',
-          ].join(' '),
-          style: Theme.of(context).textTheme.bodyMedium,
+        child: SeparatedRow(
+          separatorBuilder: (_, __) => const SizedBox(width: 6),
+          children: [
+            if (progress.status.isDownloading) ...[
+              SizedBox(
+                height: 18,
+                width: 18,
+                child: Padding(
+                  padding: const EdgeInsets.all(3),
+                  child: CircularProgressIndicator(
+                    value: (1 * progress.progress) / 100,
+                    strokeWidth: 2.5,
+                  ),
+                ),
+              ),
+              Text('${progress.progress}%'),
+            ] else
+              Icon(
+                downloadStatusIconOf(progress.status),
+                color: downloadStatusColorOf(
+                    progress.status, Theme.of(context).colorScheme),
+                size: 18,
+              ),
+            if (!groupByServer) ...[
+              const Text('•'),
+              Text(entry.post.serverName),
+            ],
+          ],
         ),
       ),
       leading: ExtendedImage.network(

pubspec.yaml
@@ -62,6 +62,7 @@ dependencies:
   permission_handler: ^10.0.0
   retry: ^3.1.0
   scroll_to_index: ^3.0.1
+  separated_row: ^2.0.0
   settings_ui: ^2.0.2
   shimmer: ^2.0.0
   tinycolor2: ^2.1.0

commit 2b6c858bdb83d2fad7872e03d2a5a6c0287e1e08

DownloadsPage: Show the download status text again
Using only icon to describe the status is not great in terms of
UX. Let's add it back.

Fixes: 71849fea ("DownloadsPage: Improve entry description layout")

lib/views/containers/downloads.dart
@@ -1,3 +1,4 @@
+import 'package:dartx/dartx.dart';
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -249,7 +250,9 @@ class _DownloadEntryView extends ConsumerWidget {
                 size: 18,
               ),
             if (progress.status.isDownloaded && !entry.isFileExists)
-              const Text('File moved or missing'),
+              const Text('File moved or missing')
+            else
+              Text(progress.status.name.capitalize()),
             if (!groupByServer) ...[
               const Text('•'),
               Text(entry.post.serverName),

pubspec.yaml
@@ -70,6 +70,7 @@ dependencies:
   video_player: ^2.2.5
   xml2json: ^5.0.0
   yaml: ^3.1.0
+  dartx: ^1.1.0
 
 dev_dependencies:
   flutter_test: