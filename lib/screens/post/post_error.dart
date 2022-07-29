3c392bc7ffae509cc218bcafaf4a6d30d53c56c6
Nauval Rizky <enuma.alrizky@gmail.com>
2022-07-30 00:43:40 +0700

    PostError: match layout to the PostImage's loading and error widget

--- lib/screens/post/post_error.dart
+++ lib/screens/post/post_error.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 import '../../../data/post.dart';
 import '../../../providers/settings/blur_explicit_post.dart';
+import '../../utils/extensions/buildcontext.dart';
 import '../../utils/extensions/string.dart';
 import 'post_placeholder_image.dart';
 
@@ -13,25 +15,45 @@ class PostErrorDisplay extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final blurExplicitPost = ref.watch(blurExplicitPostProvider);
+    final shouldBlur = ref.watch(blurExplicitPostProvider);
     return Stack(
+      alignment: Alignment.center,
+      fit: StackFit.passthrough,
       children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            PostPlaceholderImage(
-                url: post.previewFile, shouldBlur: blurExplicitPost),
-            Card(
-              margin: const EdgeInsets.fromLTRB(16, 32, 16, 32),
-              child: Padding(
-                padding: const EdgeInsets.all(1),
-                child: Text(
-                  '${post.contentFile.mimeType} is unsupported at the moment',
-                  textAlign: TextAlign.center,
-                ),
+        PostPlaceholderImage(
+          url: post.previewFile,
+          shouldBlur: shouldBlur && post.rating == PostRating.explicit,
+        ),
+        Positioned(
+          bottom: MediaQuery.of(context).padding.bottom,
+          child: Transform.scale(
+            scale: 0.9,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(Radius.circular(30)),
+                color: context.theme.cardColor,
+              ),
+              padding: const EdgeInsets.fromLTRB(8, 4, 8, 4),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Text(
+                        '${post.contentFile.fileExtension} is not supported'),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(elevation: 0),
+                    onPressed: () {
+                      launchUrlString(post.originalFile,
+                          mode: LaunchMode.externalApplication);
+                    },
+                    child: const Text('Open externally'),
+                  ),
+                ],
               ),
             ),
-          ],
+          ),
         ),
       ],
     );
