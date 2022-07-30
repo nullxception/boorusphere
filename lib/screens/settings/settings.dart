963c291634b68cb5bdba1b1b9246a2e5015ce59d
Nauval Rizky <enuma.alrizky@gmail.com>
2022-07-31 03:02:02 +0700

    SettingsPage: Add option to clear image cache

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -1,3 +1,4 @@
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -21,6 +22,7 @@ class SettingsPage extends HookConsumerWidget {
     final themeSettings = SettingsThemeData(
         titleTextColor: context.colorScheme.primary,
         settingsListBackground: Colors.transparent);
+    final messenger = ScaffoldMessenger.of(context);
 
     return Scaffold(
       appBar: AppBar(title: const Text('Settings')),
@@ -81,6 +83,24 @@ class SettingsPage extends HookConsumerWidget {
                 initialValue: safeMode,
                 onToggle: (value) {
                   ref.read(safeModeProvider.notifier).enable(value);
+                },
+              ),
+            ],
+          ),
+          SettingsSection(
+            title: const Text('Miscellaneous'),
+            tiles: [
+              SettingsTile(
+                title: const Text('Clear cache'),
+                description: const Text('Clear loaded content from cache'),
+                leading: const Icon(Icons.delete),
+                onPressed: (context) async {
+                  messenger.showSnackBar(const SnackBar(
+                    content: Text('Clearing...'),
+                    duration: Duration(seconds: 1),
+                  ));
+                  await clearDiskCachedImages();
+                  clearMemoryImageCache();
                 },
               ),
             ],
