1cc4fd288a4039d55560a43b47afd0fe34f82b47
Nauval Rizky <enuma.alrizky@gmail.com>
2022-08-06 18:55:11 +0700

    PostImageDisplay: unblur the placeholder too when show button is clicked

--- lib/screens/post/post_image.dart
+++ lib/screens/post/post_image.dart
@@ -18,7 +18,8 @@ class PostImageDisplay extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final blurExplicitPost = ref.watch(blurExplicitPostProvider);
+    final blurExplicit = ref.watch(blurExplicitPostProvider);
+    final isBlur = useState(post.rating == PostRating.explicit && blurExplicit);
     final zoomController =
         useAnimationController(duration: const Duration(milliseconds: 150));
     final zoomAnimation = useState<Animation<double>?>(null);
@@ -29,82 +30,63 @@ class PostImageDisplay extends HookConsumerWidget {
       onTap: () {
         ref.read(fullscreenProvider.notifier).toggle();
       },
-      child: PostImageBlurExplicitView(
-        post: post,
-        shouldBlur: blurExplicitPost,
-        children: ExtendedImage.network(
-          post.contentFile,
-          headers: {'Referer': post.postUrl},
-          fit: BoxFit.contain,
-          mode: ExtendedImageMode.gesture,
-          initGestureConfigHandler: (state) => GestureConfig(inPageView: true),
-          handleLoadingProgress: true,
-          loadStateChanged: (state) {
-            switch (state.extendedImageLoadState) {
-              case LoadState.loading:
-              case LoadState.failed:
-                return PostImageStatusView(
-                  post: post,
-                  state: state,
-                  shouldBlur: blurExplicitPost,
-                );
-              default:
-                return state.completedWidget;
-            }
-          },
-          onDoubleTap: (state) {
-            final downOffset = state.pointerDownPosition;
-            final begin = state.gestureDetails?.totalScale ?? 1;
-            zoomAnimation.value?.removeListener(zoomStateCallback.value!);
+      child: Stack(
+        alignment: Alignment.center,
+        fit: StackFit.passthrough,
+        children: [
+          if (isBlur.value)
+            PostPlaceholderImage(post: post, shouldBlur: true)
+          else
+            ExtendedImage.network(
+              post.contentFile,
+              headers: {'Referer': post.postUrl},
+              fit: BoxFit.contain,
+              mode: ExtendedImageMode.gesture,
+              initGestureConfigHandler: (state) =>
+                  GestureConfig(inPageView: true),
+              handleLoadingProgress: true,
+              loadStateChanged: (state) {
+                switch (state.extendedImageLoadState) {
+                  case LoadState.loading:
+                  case LoadState.failed:
+                    return PostImageStatusView(
+                      post: post,
+                      state: state,
+                    );
+                  default:
+                    return state.completedWidget;
+                }
+              },
+              onDoubleTap: (state) {
+                final downOffset = state.pointerDownPosition;
+                final begin = state.gestureDetails?.totalScale ?? 1;
+                zoomAnimation.value?.removeListener(zoomStateCallback.value!);
 
-            zoomController.stop();
-            zoomController.reset();
+                zoomController.stop();
+                zoomController.reset();
 
-            zoomStateCallback.value = () {
-              state.handleDoubleTap(
-                  scale: zoomAnimation.value?.value,
-                  doubleTapPosition: downOffset);
-            };
-            zoomAnimation.value = zoomController
-                .drive(Tween<double>(begin: begin, end: begin == 1 ? 2 : 1));
-            zoomAnimation.value?.addListener(zoomStateCallback.value!);
-            zoomController.forward();
-          },
-        ),
+                zoomStateCallback.value = () {
+                  state.handleDoubleTap(
+                      scale: zoomAnimation.value?.value,
+                      doubleTapPosition: downOffset);
+                };
+                zoomAnimation.value = zoomController.drive(
+                    Tween<double>(begin: begin, end: begin == 1 ? 2 : 1));
+                zoomAnimation.value?.addListener(zoomStateCallback.value!);
+                zoomController.forward();
+              },
+            ),
+          if (isBlur.value)
+            Center(
+              child: PostExplicitWarningCard(
+                onConfirm: () {
+                  isBlur.value = false;
+                },
+              ),
+            ),
+        ],
       ),
     );
-  }
-}
-
-class PostImageBlurExplicitView extends HookWidget {
-  const PostImageBlurExplicitView({
-    super.key,
-    required this.post,
-    required this.shouldBlur,
-    required this.children,
-  });
-
-  final Post post;
-  final bool shouldBlur;
-  final Widget children;
-
-  @override
-  Widget build(BuildContext context) {
-    final isBlur = useState(post.rating == PostRating.explicit && shouldBlur);
-    return isBlur.value
-        ? Stack(
-            alignment: Alignment.center,
-            fit: StackFit.passthrough,
-            children: [
-              PostPlaceholderImage(post: post, shouldBlur: true),
-              Center(
-                child: PostExplicitWarningCard(onConfirm: () {
-                  isBlur.value = false;
-                }),
-              ),
-            ],
-          )
-        : children;
   }
 }
 
@@ -113,12 +95,10 @@ class PostImageStatusView extends StatelessWidget {
     super.key,
     required this.post,
     required this.state,
-    required this.shouldBlur,
   });
 
   final Post post;
   final ExtendedImageState state;
-  final bool shouldBlur;
 
   @override
   Widget build(BuildContext context) {
@@ -128,10 +108,7 @@ class PostImageStatusView extends StatelessWidget {
       alignment: Alignment.center,
       fit: StackFit.passthrough,
       children: [
-        PostPlaceholderImage(
-          post: post,
-          shouldBlur: shouldBlur && post.rating == PostRating.explicit,
-        ),
+        PostPlaceholderImage(post: post, shouldBlur: false),
         Positioned(
           bottom: MediaQuery.of(context).padding.bottom,
           child: AnimatedSwitcher(
