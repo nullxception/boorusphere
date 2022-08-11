nullxception/boorusphere

commit 898e907be3d93c155909866e87af20f239f15ce9

StringExt: add toWordList() extension method
cleanup unnecessary whitespaces and split them into List<String>.

lib/utils/extensions/string.dart
@@ -27,4 +27,8 @@ extension StringExt on String {
   String get fileExtension {
     return extension(File(this).path).replaceFirst('.', '');
   }
+
+  List<String> toWordList() {
+    return replaceAll(RegExp('\\s+'), ' ').trim().split(' ');
+  }
 }

lib/screens/home/search/controller.dart
@@ -42,9 +42,9 @@ class SearchBarController extends ChangeNotifier {
   }
 
   void append(String suggested) {
-    final queries = _textController.text.replaceAll('  ', ' ').split(' ');
+    final queries = _textController.text.toWordList();
     final result = queries.sublist(0, queries.length - 1).toSet()
-      ..addAll(suggested.split(' '));
+      ..addAll(suggested.toWordList());
     text = '${result.join(' ')} ';
   }
 

lib/screens/home/search/search.dart
@@ -12,6 +12,7 @@ import '../../../source/page.dart';
 import '../../../source/search_history.dart';
 import '../../../source/suggestion.dart';
 import '../../../utils/extensions/buildcontext.dart';
+import '../../../utils/extensions/string.dart';
 import '../../../widgets/exception_info.dart';
 import '../../../widgets/favicon.dart';
 import '../home.dart';

lib/screens/post/post_detail.dart
@@ -235,7 +235,7 @@ class PostDetailsPage extends HookConsumerWidget with ClipboardMixins {
             onTap: () {
               final selectedTags = selectedtag.value;
               if (selectedTags.isNotEmpty) {
-                final tags = Set<String>.from(pageQuery.split(' '));
+                final tags = Set<String>.from(pageQuery.toWordList());
                 tags.addAll(selectedTags);
                 ref.read(pageOptionProvider.notifier).update(
                     (state) => PageOption(query: tags.join(' '), clear: true));

lib/screens/tags_blocker/tags_blocker.dart
@@ -4,6 +4,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../widgets/notice_card.dart';
 import '../../source/blocked_tags.dart';
+import '../../utils/extensions/string.dart';
 
 class TagsBlockerPage extends HookConsumerWidget {
   const TagsBlockerPage({super.key});
@@ -43,7 +44,7 @@ class TagsBlockerPage extends HookConsumerWidget {
                   child: TextFormField(
                     controller: blockedTagsController,
                     onFieldSubmitted: (value) {
-                      final values = value.trim().split(' ');
+                      final values = value.toWordList();
                       blockedTagsHandler.pushAll(values);
                       updateTags(blockedTagsHandler, blockedTags);
                       blockedTagsController.clear();

lib/source/search_history.dart
@@ -3,6 +3,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../settings/active_server.dart';
 import '../entity/search_history.dart';
+import '../utils/extensions/string.dart';
 
 final searchHistoryProvider =
     StateNotifierProvider<SearchHistorySource, Map<int, SearchHistory>>(
@@ -18,7 +19,7 @@ class SearchHistorySource extends StateNotifier<Map<int, SearchHistory>> {
   Map<int, SearchHistory> get all => Map.castFrom(_box.toMap());
 
   void rebuild(String query) {
-    final queries = query.split(' ');
+    final queries = query.toWordList();
 
     if (query.endsWith(' ') || query.isEmpty) {
       state = all;

lib/source/suggestion.dart
@@ -33,7 +33,7 @@ class SuggestionSource {
   }) async {
     final blockedTags = ref.read(blockedTagsProvider);
 
-    final url = server.suggestionUrlOf(query.split(' ').last);
+    final url = server.suggestionUrlOf(query.toWordList().last);
     try {
       final res = await retryFuture(
         () => http.get(url.asUri).timeout(const Duration(seconds: 5)),

lib/utils/extensions/map.dart
@@ -1,6 +1,8 @@
 import 'dart:collection';
 import 'dart:convert';
 
+import 'string.dart';
+
 extension LinkedHashMapExt on LinkedHashMap {
   String toGDataString() {
     try {
@@ -21,7 +23,7 @@ extension MapStringExt on Map<String, dynamic> {
         if (orElse is int) {
           return int.parse(g) as T;
         } else if (orElse is List) {
-          return g.trim().split(' ') as T;
+          return g.toWordList() as T;
         }
         return g as T;
       }
@@ -29,7 +31,7 @@ extension MapStringExt on Map<String, dynamic> {
       if (orElse is int && data is! int) {
         return int.parse(data) as T;
       } else if (orElse is List && data is String) {
-        return data.trim().split(' ') as T;
+        return data.toWordList() as T;
       }
       return data as T;
     } catch (e) {

commit 4546dd32c07b51e137e9ca0c428c903a0a4b6037

StringExt: toWordList(): make sure that list has no empty string
Fixes: 898e907be3

lib/utils/extensions/string.dart
@@ -27,6 +27,10 @@ extension StringExt on String {
   }
 
   List<String> toWordList() {
-    return replaceAll(RegExp('\\s+'), ' ').trim().split(' ');
+    return replaceAll(RegExp('\\s+'), ' ')
+        .trim()
+        .split(' ')
+        .where((it) => it.isNotEmpty)
+        .toList();
   }
 }