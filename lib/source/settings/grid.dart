e79e8d8ce3f36c10e3dd1e2bb207c11037b6bd4d
Nauval Rizky <enuma.alrizky@gmail.com>
2022-08-20 22:22:06 +0700

    GridState: use flex-grid+1 by default

--- lib/source/settings/grid.dart
+++ lib/source/settings/grid.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'settings.dart';
 
 final gridProvider = StateNotifierProvider<GridState, int>((ref) {
-  final saved = Settings.timeline_grid_number.read(or: 0);
+  final saved = Settings.timeline_grid_number.read(or: 1);
   return GridState(saved);
 });
 
