678f7df3e43d2862d8c1c74c1cf7f85f80788d4d
Nauval Rizky <enuma.alrizky@gmail.com>
2022-08-23 10:17:18 +0700

    pubspec: reorganize dependencies

--- lib/constants/licenses.g.dart
+++ lib/constants/licenses.g.dart
@@ -1524,39 +1524,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.''',
     isDirectDependency: true,
   ),
   Package(
-    name: 'effective_dart',
-    description:
-        'Linter rules corresponding to the guidelines in Effective Dart.',
-    homepage: 'https://github.com/tenhobi/effective_dart',
-    repository: 'https://github.com/tenhobi/effective_dart',
-    authors: [],
-    version: '1.3.2',
-    license: '''MIT License
-
-Copyright (c) 2020 Honza Bittner
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.''',
-    isMarkdown: false,
-    isSdk: false,
-    isDirectDependency: false,
-  ),
-  Package(
     name: 'extended_image',
     description:
         'Official extension image, support placeholder(loading)/ failed state, cache network, zoom/pan, photo view, slide out page, editor(crop,rotate,flip), painting etc.',
@@ -2767,7 +2734,7 @@ See the License for the specific language governing permissions and
 limitations under the License.''',
     isMarkdown: false,
     isSdk: false,
-    isDirectDependency: true,
+    isDirectDependency: false,
   ),
   Package(
     name: 'hooks_riverpod',
@@ -3166,7 +3133,7 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.''',
     isMarkdown: false,
     isSdk: false,
-    isDirectDependency: true,
+    isDirectDependency: false,
   ),
   Package(
     name: 'lint',
