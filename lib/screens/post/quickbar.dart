05cfb9823f33ba23bc52b6bba3ede2e68ec7d1d5
Nauval Rizky <enuma.alrizky@gmail.com>
2022-08-27 00:45:07 +0700

    quickbar: Improve layout consistency

--- lib/screens/post/quickbar.dart
+++ lib/screens/post/quickbar.dart
@@ -53,54 +53,61 @@ class QuickBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: const BorderRadius.all(Radius.circular(30)),
-        color: context.theme.cardColor,
+    return ConstrainedBox(
+      constraints: const BoxConstraints(
+        minWidth: 50,
+        minHeight: 50,
+        maxHeight: 50,
       ),
-      padding: const EdgeInsets.fromLTRB(4, 1, 4, 1),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          if (title != null)
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: Material(
-                textStyle: context.theme.textTheme.bodySmall,
-                type: MaterialType.transparency,
-                child: title!,
-              ),
-            ),
-          if (actionTitle != null)
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                elevation: 0,
-                textStyle: context.theme.textTheme.bodySmall,
-              ),
-              onPressed: onPressed,
-              child: actionTitle!,
-            ),
-          if (useProgressBar)
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.all(Radius.circular(30)),
-                color: context.theme.colorScheme.background,
-              ),
-              padding: const EdgeInsets.all(8),
-              margin: const EdgeInsets.all(2),
-              child: SizedBox(
-                width: 21,
-                height: 21,
-                child: CircularProgressIndicator(
-                  strokeWidth: 3,
-                  valueColor: const AlwaysStoppedAnimation(
-                    Colors.white54,
-                  ),
-                  value: progress,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color: context.theme.cardColor,
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 5),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (title != null)
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Material(
+                  textStyle: context.theme.textTheme.bodySmall,
+                  type: MaterialType.transparency,
+                  child: title!,
                 ),
               ),
-            ),
-        ],
+            if (actionTitle != null)
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  textStyle: context.theme.textTheme.bodySmall,
+                ),
+                onPressed: onPressed,
+                child: actionTitle!,
+              ),
+            if (useProgressBar)
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: context.theme.colorScheme.background,
+                ),
+                padding: const EdgeInsets.all(8),
+                margin: const EdgeInsets.all(2),
+                child: SizedBox(
+                  width: 21,
+                  height: 21,
+                  child: CircularProgressIndicator(
+                    strokeWidth: 3,
+                    valueColor: AlwaysStoppedAnimation(
+                      context.colorScheme.primary.withOpacity(0.75),
+                    ),
+                    value: progress,
+                  ),
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
