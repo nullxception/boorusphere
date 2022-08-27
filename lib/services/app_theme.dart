487cbc7570f786a62480b5de0ad56bf3fb00969d
Nauval Rizky <enuma.alrizky@gmail.com>
2022-08-28 00:53:51 +0700

    AppThemeService: simplify themedata customization

--- lib/services/app_theme.dart
+++ lib/services/app_theme.dart
@@ -18,40 +18,48 @@ class AppThemeData {
 }
 
 class AppThemeService {
-  late AppThemeData _data = _createData();
+  late AppThemeData _data = _createAppThemeData();
 
   AppThemeData get data => _data;
 
   AppThemeData fillWith({ColorScheme? light, ColorScheme? dark}) {
-    _data = _createData(light: light, dark: dark);
+    _data = _createAppThemeData(light: light, dark: dark);
     return _data;
   }
 
-  AppThemeData _createData({ColorScheme? light, ColorScheme? dark}) {
+  AppThemeData _createAppThemeData({ColorScheme? light, ColorScheme? dark}) {
     return AppThemeData(
-      day: _createDay(light),
-      night: _createNight(dark),
-      midnight: _createMidnight(dark),
+      day: _createThemeData(light, Brightness.light),
+      night: _createThemeData(dark, Brightness.dark),
+      midnight: _createThemeDataMidnight(dark),
     );
   }
 
-  ThemeData _createDay(ColorScheme? scheme) {
-    final colorScheme = scheme?.harmonized() ?? defaultLightScheme;
-    final origin = ThemeData.light();
+  ThemeData _createThemeData(ColorScheme? scheme, Brightness brightness) {
+    final isDark = brightness == Brightness.dark;
+    final defScheme = isDark ? defDarkScheme : defLightScheme;
+    final colorScheme = scheme?.harmonized() ?? defScheme;
+    final origin = isDark ? ThemeData.dark() : ThemeData.light();
     return origin.copyWith(
       useMaterial3: true,
       colorScheme: colorScheme,
       appBarTheme: AppBarTheme(
-        backgroundColor: colorScheme.surface,
+        backgroundColor:
+            isDark ? colorScheme.surface.darken(2) : colorScheme.surface,
         foregroundColor: colorScheme.onSurface,
         elevation: 0,
       ),
       backgroundColor: colorScheme.surface,
       canvasColor: colorScheme.surface,
-      cardColor: colorScheme.surface.darken(2),
-      scaffoldBackgroundColor: colorScheme.surface,
+      cardColor: isDark
+          ? colorScheme.surface.lighten(3)
+          : colorScheme.surface.darken(2),
+      scaffoldBackgroundColor:
+          isDark ? colorScheme.surface.darken(2) : colorScheme.surface,
       drawerTheme: origin.drawerTheme.copyWith(
-        backgroundColor: colorScheme.surface.shade(3),
+        backgroundColor: isDark
+            ? colorScheme.surface.shade(40)
+            : colorScheme.surface.shade(3),
       ),
       toggleableActiveColor: colorScheme.primary,
       snackBarTheme: origin.snackBarTheme.copyWith(
@@ -71,91 +79,38 @@ class AppThemeService {
     );
   }
 
-  ThemeData _createNight(ColorScheme? scheme) {
-    final colorScheme = scheme?.harmonized() ?? defaultDarkScheme;
-    final origin = ThemeData.dark();
+  ThemeData _createThemeDataMidnight(ColorScheme? scheme) {
+    final origin = _createThemeData(scheme, Brightness.dark);
     return origin.copyWith(
-      useMaterial3: true,
-      colorScheme: colorScheme,
-      appBarTheme: AppBarTheme(
-        backgroundColor: colorScheme.surface.darken(2),
-        foregroundColor: colorScheme.onSurface,
-        elevation: 0,
-      ),
-      backgroundColor: colorScheme.surface,
-      canvasColor: colorScheme.surface,
-      cardColor: colorScheme.surface.lighten(3),
-      scaffoldBackgroundColor: colorScheme.surface.darken(2),
-      drawerTheme: origin.drawerTheme.copyWith(
-        backgroundColor: colorScheme.surface.shade(40),
-      ),
-      toggleableActiveColor: colorScheme.primary,
-      snackBarTheme: origin.snackBarTheme.copyWith(
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(11),
-            topRight: Radius.circular(11),
-          ),
-        ),
-        backgroundColor: colorScheme.primaryContainer,
-        contentTextStyle: TextStyle(color: colorScheme.onPrimaryContainer),
-      ),
-      listTileTheme: origin.listTileTheme.copyWith(
-        minVerticalPadding: 12,
-        contentPadding: const EdgeInsets.symmetric(horizontal: 22),
-      ),
-    );
-  }
-
-  ThemeData _createMidnight(ColorScheme? scheme) {
-    final colorScheme = scheme?.harmonized() ?? defaultDarkScheme;
-    final origin = ThemeData.dark();
-    return origin.copyWith(
-      useMaterial3: true,
-      colorScheme: colorScheme.copyWith(
+      colorScheme: origin.colorScheme.copyWith(
         brightness: Brightness.dark,
-        surface: colorScheme.background,
+        surface: origin.colorScheme.background,
         background: Colors.black,
       ),
       appBarTheme: AppBarTheme(
         backgroundColor: Colors.black,
-        foregroundColor: colorScheme.onSurface,
+        foregroundColor: origin.colorScheme.onSurface,
         elevation: 0,
       ),
       primaryColor: Colors.black,
       backgroundColor: Colors.black,
       canvasColor: Colors.black,
-      cardColor: colorScheme.background.darken(3),
+      cardColor: origin.colorScheme.background.darken(3),
       drawerTheme: origin.drawerTheme.copyWith(
-        backgroundColor: Colors.black.brighten(6),
+        backgroundColor: Colors.black,
       ),
       scaffoldBackgroundColor: Colors.black,
-      toggleableActiveColor: colorScheme.primary,
-      snackBarTheme: origin.snackBarTheme.copyWith(
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(11),
-            topRight: Radius.circular(11),
-          ),
-        ),
-        backgroundColor: colorScheme.primaryContainer,
-        contentTextStyle: TextStyle(color: colorScheme.onPrimaryContainer),
-      ),
-      listTileTheme: origin.listTileTheme.copyWith(
-        minVerticalPadding: 12,
-        contentPadding: const EdgeInsets.symmetric(horizontal: 22),
-      ),
     );
   }
 
   static const defaultAccent = Color.fromARGB(255, 149, 30, 229);
 
-  static final defaultLightScheme = ColorScheme.fromSeed(
+  static final defLightScheme = ColorScheme.fromSeed(
     seedColor: defaultAccent,
     brightness: Brightness.light,
   );
 
-  static final defaultDarkScheme = ColorScheme.fromSeed(
+  static final defDarkScheme = ColorScheme.fromSeed(
     seedColor: defaultAccent,
     brightness: Brightness.dark,
   );
