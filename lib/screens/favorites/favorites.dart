71dda6a3709947e8bb64caeb44bfe97086e49c7e
Nauval Rizky <enuma.alrizky@gmail.com>
2022-08-31 04:10:58 +0700

    favorites: move tab to bottom and improve mapping

--- lib/screens/favorites/favorites.dart
+++ lib/screens/favorites/favorites.dart
@@ -1,13 +1,15 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../entity/post.dart';
 import '../../entity/server_data.dart';
 import '../../source/favorites.dart';
 import '../../source/server.dart';
 import '../../utils/extensions/buildcontext.dart';
 import '../../widgets/favicon.dart';
 import '../../widgets/notice_card.dart';
-import '../../widgets/preferred_size_builder.dart';
 import '../home/timeline/controller.dart';
 import '../home/timeline/timeline.dart';
 
@@ -18,106 +20,6 @@ class FavoritesPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final hasFav = ref.watch(favoritesProvider.select((it) => it.isNotEmpty));
     return hasFav ? _FavoritesView() : _EmptyView();
-  }
-}
-
-class _FavoritesView extends HookConsumerWidget {
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final servers = ref.watch(favoritesProvider.select(
-      (it) => it.values
-          .map((e) => ref
-              .watch(serverDataProvider.notifier)
-              .getByName(e.post.serverName, or: ServerData.empty))
-          .where((e) => e != ServerData.empty)
-          .toSet(),
-    ));
-
-    return DefaultTabController(
-      length: servers.length,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Favorites'),
-          bottom: PreferredSizeBuilder(
-            builder: (context, child) => Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 8),
-                child: child,
-              ),
-            ),
-            child: TabBar(
-              isScrollable: true,
-              tabs: [
-                for (final server in servers) _FavoriteTab(serverData: server),
-              ],
-              indicator: BoxDecoration(
-                color: context.colorScheme.primaryContainer,
-                borderRadius: BorderRadius.circular(16),
-              ),
-            ),
-          ),
-        ),
-        body: TabBarView(
-          children: [
-            for (final server in servers) _FavoriteTimeline(serverData: server),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class _FavoriteTab extends ConsumerWidget {
-  const _FavoriteTab({required this.serverData});
-
-  final ServerData serverData;
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Row(
-      children: [
-        Favicon(
-          url: '${serverData.homepage}/favicon.ico',
-          size: 12,
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8),
-          child: Text(serverData.name),
-        ),
-      ],
-    );
-  }
-}
-
-class _FavoriteTimeline extends HookConsumerWidget {
-  const _FavoriteTimeline({required this.serverData});
-
-  final ServerData serverData;
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final posts = ref.watch(favoritesProvider.select(
-      (value) => value.values
-          .where((it) => it.post.serverName == serverData.name)
-          .map((it) => it.post),
-    ));
-    final controller = useTimelineController(
-      posts: posts.toList(),
-      heroKeyBuilder: (post) => 'fav@${serverData.name}-${post.id}',
-    );
-
-    return CustomScrollView(
-      controller: controller.scrollController,
-      slivers: [
-        SliverSafeArea(
-          sliver: SliverPadding(
-            padding: const EdgeInsets.all(10),
-            sliver: Timeline(controller: controller),
-          ),
-        ),
-      ],
-    );
   }
 }
 
@@ -141,6 +43,115 @@ class _EmptyView extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class _FavoritesView extends HookConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final grouped = ref.watch(favoritesProvider.select(
+      (it) => it.values.groupListsBy((e) => ref
+          .watch(serverDataProvider.notifier)
+          .getByName(e.post.serverName, or: ServerData.empty)),
+    ));
+
+    final entries = grouped.entries
+        .where((it) => it.key != ServerData.empty)
+        .sortedBy((it) => it.key.name);
+    final servers = entries.map((e) => e.key);
+    final pages = entries.map(
+      (e) => MapEntry(
+        e.key,
+        e.value.map((e) => e.post).toList(),
+      ),
+    );
+
+    return DefaultTabController(
+      length: servers.length,
+      child: Scaffold(
+        extendBody: true,
+        appBar: AppBar(
+          title: const Text('Favorites'),
+        ),
+        body: TabBarView(
+          children: [
+            for (final page in pages)
+              _Content(serverData: page.key, data: page.value),
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: context.colorScheme.background.withOpacity(0.97),
+          elevation: 0,
+          child: TabBar(
+            labelStyle: const TextStyle(fontSize: 12),
+            padding: const EdgeInsets.all(8),
+            isScrollable: true,
+            tabs: [
+              for (final server in servers) _Tab(serverData: server),
+            ],
+            indicator: BoxDecoration(
+              color: context.colorScheme.primaryContainer,
+              borderRadius: BorderRadius.circular(16),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Tab extends StatelessWidget {
+  const _Tab({required this.serverData});
+
+  final ServerData serverData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Favicon(
+            url: '${serverData.homepage}/favicon.ico',
+            size: 16,
+            shape: BoxShape.circle,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Text(serverData.name),
+        ),
+      ],
+    );
+  }
+}
+
+class _Content extends HookWidget {
+  const _Content({required this.data, required this.serverData});
+
+  final List<Post> data;
+  final ServerData serverData;
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = useTimelineController(
+      posts: data,
+      heroKeyBuilder: (post) => 'fav@$serverData-${post.id}',
+      keys: [data],
+    );
+
+    return CustomScrollView(
+      controller: controller.scrollController,
+      slivers: [
+        SliverSafeArea(
+          sliver: SliverPadding(
+            padding: const EdgeInsets.all(10),
+            sliver: Timeline(controller: controller),
+          ),
+        ),
+      ],
     );
   }
 }
