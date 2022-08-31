b4d7f168a6fa0299365aa00a49e7ccc1b9e9879e
Nauval Rizky <enuma.alrizky@gmail.com>
2022-09-01 02:13:07 +0700

    Add animation to the fav button and remove snackbar
    snackbar is too intrusive

--- lib/screens/post/post_toolbox.dart
+++ lib/screens/post/post_toolbox.dart
@@ -88,61 +88,7 @@ class PostOpenLinkButton extends StatelessWidget {
   }
 }
 
-class FavoriteSnackbar extends StatelessWidget {
-  const FavoriteSnackbar({super.key, required this.isAdded});
-
-  final bool isAdded;
-
-  @override
-  Widget build(BuildContext context) {
-    final bottomSafe =
-        context.mediaQuery.viewPadding.bottom + kBottomNavigationBarHeight;
-
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        GestureDetector(
-          behavior: HitTestBehavior.translucent,
-          child: Container(
-            decoration: BoxDecoration(
-              color: context.theme.snackBarTheme.backgroundColor,
-              borderRadius: BorderRadius.circular(24),
-            ),
-            padding: const EdgeInsets.fromLTRB(16, 12, 16, 12),
-            margin: EdgeInsets.fromLTRB(16, 0, 16, bottomSafe),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Icon(Icons.favorite_outline, size: 16),
-                const SizedBox(width: 8),
-                Text(isAdded ? 'Added to Favorites' : 'Removed from Favorites'),
-              ],
-            ),
-          ),
-          onTap: () {
-            context.scaffoldMessenger.hideCurrentSnackBar();
-          },
-        ),
-      ],
-    );
-  }
-
-  static void show({required BuildContext context, required bool isAdded}) {
-    context.scaffoldMessenger.showSnackBar(
-      SnackBar(
-        backgroundColor: Colors.transparent,
-        content: FavoriteSnackbar(isAdded: isAdded),
-        padding: EdgeInsets.zero,
-        behavior: SnackBarBehavior.floating,
-        dismissDirection: DismissDirection.horizontal,
-        duration: const Duration(seconds: 1),
-      ),
-    );
-  }
-}
-
-class PostFavoriteButton extends ConsumerWidget {
+class PostFavoriteButton extends HookConsumerWidget {
   const PostFavoriteButton({
     super.key,
     required this.post,
@@ -154,23 +100,25 @@ class PostFavoriteButton extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     ref.watch(favoritesProvider);
     final isFavorite = ref.watch(favoritesProvider.notifier).checkExists(post);
+    final animator =
+        useAnimationController(duration: const Duration(milliseconds: 300));
+    final animation = useAnimation(ColorTween(
+            begin: context.iconTheme.color ?? context.colorScheme.onSurface,
+            end: Colors.pink.shade300)
+        .animate(animator));
+    isFavorite ? animator.forward() : animator.reverse();
 
     return IconButton(
       padding: const EdgeInsets.all(16),
       color: Colors.white,
-      icon: AnimatedSwitcher(
-        duration: kThemeChangeDuration,
-        child: isFavorite
-            ? const Icon(Icons.favorite)
-            : const Icon(Icons.favorite_border),
-      ),
+      icon: isFavorite
+          ? Icon(Icons.favorite, color: animation)
+          : Icon(Icons.favorite_border, color: animation),
       onPressed: () {
         if (isFavorite) {
           ref.watch(favoritesProvider.notifier).delete(post);
-          FavoriteSnackbar.show(context: context, isAdded: false);
         } else {
           ref.watch(favoritesProvider.notifier).save(post);
-          FavoriteSnackbar.show(context: context, isAdded: true);
         }
       },
     );
