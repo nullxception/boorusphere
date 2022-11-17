dd49176f5427f929b4521f65936ceaa551286730
Nauval Rizky <enuma.alrizky@gmail.com>
2022-11-17 08:26:31 +0700

    domain: generate providers

--- lib/domain/provider.dart
+++ lib/domain/provider.dart
@@ -16,57 +16,65 @@ import 'package:boorusphere/domain/repository/search_history_repo.dart';
 import 'package:boorusphere/domain/repository/server_repo.dart';
 import 'package:boorusphere/domain/repository/setting_repo.dart';
 import 'package:boorusphere/domain/repository/version_repo.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-final blockedTagsRepoProvider = Provider.autoDispose<BlockedTagsRepo>((ref) {
+part 'provider.g.dart';
+
+@riverpod
+BlockedTagsRepo blockedTagsRepo(BlockedTagsRepoRef ref) {
   return BlockedTagsRepoImpl(
     localSource: ref.watch(blockedTagsLocalSourceProvider),
   );
-});
+}
 
-final booruRepoProvider =
-    Provider.autoDispose.family<BooruRepo, ServerData>((ref, server) {
+@riverpod
+BooruRepo booruRepo(BooruRepoRef ref, ServerData server) {
   return BooruRepoImpl(
     networkSource: ref.watch(booruNetworkSourceProvider),
     server: server,
   );
-});
+}
 
-final changelogRepoProvider = Provider.autoDispose<ChangelogRepo>((ref) {
+@riverpod
+ChangelogRepo changelogRepo(ChangelogRepoRef ref) {
   return ChangelogRepoImpl(
     localSource: ref.watch(changelogLocalSourceProvider),
     networkSource: ref.watch(changelogNetworkSourceProvider),
   );
-});
+}
 
-final favoritePostRepoProvider = Provider.autoDispose<FavoritePostRepo>((ref) {
+@riverpod
+FavoritePostRepo favoritePostRepo(FavoritePostRepoRef ref) {
   return FavoritePostRepoImpl(
     localSource: ref.watch(favoritePostLocalSourceProvider),
   );
-});
+}
 
-final searchHistoryRepoProvider =
-    Provider.autoDispose<SearchHistoryRepo>((ref) {
+@riverpod
+SearchHistoryRepo searchHistoryRepo(SearchHistoryRepoRef ref) {
   return SearchHistoryRepoImpl(
     localSource: ref.watch(searchHistoryLocalSourceProvider),
   );
-});
+}
 
-final serverRepoProvider = Provider.autoDispose<ServerRepo>((ref) {
+@riverpod
+ServerRepo serverRepo(ServerRepoRef ref) {
   return ServerRepoImpl(
     localSource: ref.watch(serverLocalSourceProvider),
   );
-});
+}
 
-final settingRepoProvider = Provider.autoDispose<SettingRepo>((ref) {
+@riverpod
+SettingRepo settingRepo(SettingRepoRef ref) {
   return SettingRepoImpl(
-    localSource: ref.watch(settingsLocalSourceProvider),
+    localSource: ref.watch(settingLocalSourceProvider),
   );
-});
+}
 
-final versionRepoProvider = Provider.autoDispose<VersionRepo>((ref) {
+@riverpod
+VersionRepo versionRepo(VersionRepoRef ref) {
   return VersionRepoImpl(
     localSource: ref.watch(versionLocalSourceProvider),
     networkSource: ref.watch(versionNetworkSourceProvider),
   );
-});
+}
