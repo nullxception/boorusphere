a1f74117d632dc4e3390d1b44079ce90fb06dd38
Nauval Rizky <enuma.alrizky@gmail.com>
2022-12-01 12:24:34 +0700

    SettingsPage: extract individual settings into it's own widget

--- lib/presentation/screens/settings/settings_page.dart
+++ lib/presentation/screens/settings/settings_page.dart
@@ -10,6 +10,7 @@ import 'package:boorusphere/utils/download.dart';
 import 'package:extended_image/extended_image.dart' as extended_image;
 import 'package:flutter/material.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class SettingsPage extends StatelessWidget {
@@ -19,215 +20,46 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(context.t.settings.title)),
-      body: const SafeArea(
-        child: _SettingsContent(),
+      body: SafeArea(
+        child: ListView(
+          children: [
+            _Section(
+              title: Text(context.t.downloads.title),
+              children: const [
+                _HideMedia(),
+              ],
+            ),
+            _Section(
+              title: Text(context.t.settings.interface),
+              children: const [
+                _Language(),
+                _MidnightMode(),
+                _UiBlur(),
+              ],
+            ),
+            _Section(
+              title: Text(context.t.settings.safeMode),
+              children: const [
+                _BlurContent(),
+                _StrictSafeMode(),
+              ],
+            ),
+            _Section(
+              title: Text(context.t.servers.title),
+              children: const [
+                _LoadOriginal(),
+                _PostLimit(),
+              ],
+            ),
+            _Section(
+              title: Text(context.t.settings.misc),
+              children: const [
+                _ClearCache(),
+              ],
+            ),
+          ],
+        ),
       ),
-    );
-  }
-}
-
-class _SettingsContent extends HookConsumerWidget {
-  const _SettingsContent();
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final markMayNeedRebuild = useMarkMayNeedRebuild();
-    const subtitlePadding = EdgeInsets.only(top: 8);
-
-    return ListView(
-      children: [
-        _Section(
-          title: Text(context.t.downloads.title),
-          children: [
-            FutureBuilder(
-              future: DownloadUtils.hasDotnomedia,
-              initialData: false,
-              builder: (context, snapshot) {
-                final data = snapshot.data;
-                final value = data is bool ? data : false;
-                return SwitchListTile(
-                  title: Text(context.t.settings.hideMedia.title),
-                  subtitle: Padding(
-                    padding: subtitlePadding,
-                    child: Text(context.t.settings.hideMedia.desc),
-                  ),
-                  value: value,
-                  onChanged: (isEnabled) async {
-                    isEnabled
-                        ? await DownloadUtils.createDotnomedia()
-                        : await DownloadUtils.removeDotnomedia();
-                    markMayNeedRebuild();
-                  },
-                );
-              },
-            ),
-          ],
-        ),
-        _Section(
-          title: Text(context.t.settings.interface),
-          children: [
-            ListTile(
-              title: Text(context.t.settings.lang.title),
-              subtitle: Padding(
-                padding: subtitlePadding,
-                child: _CurrentLanguage(),
-              ),
-              onTap: () {
-                context.router.push(const LanguageSettingsRoute());
-              },
-            ),
-            SwitchListTile(
-              title: Text(context.t.settings.midnightTheme.title),
-              subtitle: Padding(
-                padding: subtitlePadding,
-                child: Text(context.t.settings.midnightTheme.desc),
-              ),
-              value: ref.watch(
-                  uiSettingStateProvider.select((ui) => ui.midnightMode)),
-              onChanged: (value) {
-                ref
-                    .read(uiSettingStateProvider.notifier)
-                    .setMidnightMode(value);
-              },
-            ),
-            SwitchListTile(
-              title: Text(context.t.settings.uiBlur.title),
-              subtitle: Padding(
-                padding: subtitlePadding,
-                child: Text(context.t.settings.uiBlur.desc),
-              ),
-              value: ref.watch(uiSettingStateProvider.select((ui) => ui.blur)),
-              onChanged: (value) {
-                ref.read(uiSettingStateProvider.notifier).showBlur(value);
-              },
-            ),
-          ],
-        ),
-        _Section(
-          title: Text(context.t.settings.safeMode),
-          children: [
-            SwitchListTile(
-              title: Text(context.t.settings.blurContent.title),
-              subtitle: Padding(
-                padding: subtitlePadding,
-                child: Text(context.t.settings.blurContent.desc),
-              ),
-              value: ref.watch(
-                  contentSettingStateProvider.select((it) => it.blurExplicit)),
-              onChanged: (value) {
-                ref
-                    .read(contentSettingStateProvider.notifier)
-                    .setBlurExplicitPost(value);
-              },
-            ),
-            SwitchListTile(
-              title: Text(context.t.settings.strictSafeMode.title),
-              subtitle: Padding(
-                padding: subtitlePadding,
-                child: Text(context.t.settings.strictSafeMode.desc),
-              ),
-              value: ref.watch(
-                  serverSettingStateProvider.select((it) => it.safeMode)),
-              onChanged: (value) {
-                ref
-                    .read(serverSettingStateProvider.notifier)
-                    .setSafeMode(value);
-              },
-            ),
-          ],
-        ),
-        _Section(
-          title: Text(context.t.servers.title),
-          children: [
-            SwitchListTile(
-              title: Text(context.t.settings.loadOg.title),
-              subtitle: Padding(
-                padding: subtitlePadding,
-                child: Text(context.t.settings.loadOg.desc),
-              ),
-              value: ref.watch(
-                  contentSettingStateProvider.select((it) => it.loadOriginal)),
-              onChanged: (value) {
-                ref
-                    .read(contentSettingStateProvider.notifier)
-                    .setLoadOriginalPost(value);
-              },
-            ),
-            ListTile(
-              title: Text(context.t.settings.postLimit.title),
-              subtitle: Padding(
-                padding: subtitlePadding,
-                child: Text(context.t.settings.postLimit.desc),
-              ),
-              trailing: DropdownButton(
-                menuMaxHeight: 178,
-                value: ref.watch(
-                    serverSettingStateProvider.select((it) => it.postLimit)),
-                elevation: 1,
-                underline: const SizedBox.shrink(),
-                borderRadius: const BorderRadius.all(Radius.circular(5)),
-                items: List<DropdownMenuItem<int>>.generate(
-                  10,
-                  (i) {
-                    final x = i * 10 + 10;
-                    return DropdownMenuItem(
-                      value: x,
-                      child: Text('$x'),
-                    );
-                  },
-                ),
-                onChanged: (value) {
-                  ref
-                      .read(serverSettingStateProvider.notifier)
-                      .setPostLimit(value as int);
-                },
-              ),
-            ),
-          ],
-        ),
-        _Section(
-          title: Text(context.t.settings.misc),
-          children: [
-            ListTile(
-              title: Text(context.t.settings.clearCache.title),
-              subtitle: Padding(
-                padding: subtitlePadding,
-                child: Text(context.t.settings.clearCache.desc),
-              ),
-              onTap: () async {
-                context.scaffoldMessenger.showSnackBar(SnackBar(
-                  content: Text(context.t.clearing),
-                  duration: const Duration(milliseconds: 500),
-                ));
-
-                await DefaultCacheManager().emptyCache();
-                await extended_image.clearDiskCachedImages();
-                extended_image.clearMemoryImageCache();
-
-                context.scaffoldMessenger.showSnackBar(
-                  SnackBar(
-                    content: Text(context.t.settings.clearCache.done),
-                    duration: const Duration(milliseconds: 500),
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
-      ],
-    );
-  }
-}
-
-class _CurrentLanguage extends ConsumerWidget {
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final locale = ref.watch(uiSettingStateProvider.select((ui) => ui.locale));
-
-    return Text(
-      locale == null
-          ? context.t.settings.lang.auto.title
-          : context.t.languageName,
     );
   }
 }
@@ -256,6 +88,240 @@ class _Section extends StatelessWidget {
         ),
         ...children,
       ],
+    );
+  }
+}
+
+class _HideMedia extends HookWidget {
+  const _HideMedia();
+
+  @override
+  Widget build(BuildContext context) {
+    final markMayNeedRebuild = useMarkMayNeedRebuild();
+
+    return FutureBuilder(
+      future: DownloadUtils.hasDotnomedia,
+      initialData: false,
+      builder: (context, snapshot) {
+        final data = snapshot.data;
+        final value = data is bool ? data : false;
+        return SwitchListTile(
+          title: Text(context.t.settings.hideMedia.title),
+          subtitle: Padding(
+            padding: const EdgeInsets.only(top: 8),
+            child: Text(context.t.settings.hideMedia.desc),
+          ),
+          value: value,
+          onChanged: (isEnabled) async {
+            isEnabled
+                ? await DownloadUtils.createDotnomedia()
+                : await DownloadUtils.removeDotnomedia();
+            markMayNeedRebuild();
+          },
+        );
+      },
+    );
+  }
+}
+
+class _CurrentLanguage extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final locale = ref.watch(uiSettingStateProvider.select((ui) => ui.locale));
+
+    return Text(
+      locale == null
+          ? context.t.settings.lang.auto.title
+          : context.t.languageName,
+    );
+  }
+}
+
+class _MidnightMode extends ConsumerWidget {
+  const _MidnightMode();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return SwitchListTile(
+      title: Text(context.t.settings.midnightTheme.title),
+      subtitle: Padding(
+        padding: const EdgeInsets.only(top: 8),
+        child: Text(context.t.settings.midnightTheme.desc),
+      ),
+      value: ref.watch(uiSettingStateProvider.select((ui) => ui.midnightMode)),
+      onChanged: (value) {
+        ref.read(uiSettingStateProvider.notifier).setMidnightMode(value);
+      },
+    );
+  }
+}
+
+class _UiBlur extends ConsumerWidget {
+  const _UiBlur();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return SwitchListTile(
+      title: Text(context.t.settings.uiBlur.title),
+      subtitle: Padding(
+        padding: const EdgeInsets.only(top: 8),
+        child: Text(context.t.settings.uiBlur.desc),
+      ),
+      value: ref.watch(uiSettingStateProvider.select((ui) => ui.blur)),
+      onChanged: (value) {
+        ref.read(uiSettingStateProvider.notifier).showBlur(value);
+      },
+    );
+  }
+}
+
+class _BlurContent extends ConsumerWidget {
+  const _BlurContent();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return SwitchListTile(
+      title: Text(context.t.settings.blurContent.title),
+      subtitle: Padding(
+        padding: const EdgeInsets.only(top: 8),
+        child: Text(context.t.settings.blurContent.desc),
+      ),
+      value: ref
+          .watch(contentSettingStateProvider.select((it) => it.blurExplicit)),
+      onChanged: (value) {
+        ref
+            .read(contentSettingStateProvider.notifier)
+            .setBlurExplicitPost(value);
+      },
+    );
+  }
+}
+
+class _StrictSafeMode extends ConsumerWidget {
+  const _StrictSafeMode();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return SwitchListTile(
+      title: Text(context.t.settings.strictSafeMode.title),
+      subtitle: Padding(
+        padding: const EdgeInsets.only(top: 8),
+        child: Text(context.t.settings.strictSafeMode.desc),
+      ),
+      value: ref.watch(serverSettingStateProvider.select((it) => it.safeMode)),
+      onChanged: (value) {
+        ref.read(serverSettingStateProvider.notifier).setSafeMode(value);
+      },
+    );
+  }
+}
+
+class _Language extends StatelessWidget {
+  const _Language();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(context.t.settings.lang.title),
+      subtitle: Padding(
+        padding: const EdgeInsets.only(top: 8),
+        child: _CurrentLanguage(),
+      ),
+      onTap: () {
+        context.router.push(const LanguageSettingsRoute());
+      },
+    );
+  }
+}
+
+class _LoadOriginal extends ConsumerWidget {
+  const _LoadOriginal();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return SwitchListTile(
+      title: Text(context.t.settings.loadOg.title),
+      subtitle: Padding(
+        padding: const EdgeInsets.only(top: 8),
+        child: Text(context.t.settings.loadOg.desc),
+      ),
+      value: ref
+          .watch(contentSettingStateProvider.select((it) => it.loadOriginal)),
+      onChanged: (value) {
+        ref
+            .read(contentSettingStateProvider.notifier)
+            .setLoadOriginalPost(value);
+      },
+    );
+  }
+}
+
+class _PostLimit extends ConsumerWidget {
+  const _PostLimit();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ListTile(
+      title: Text(context.t.settings.postLimit.title),
+      subtitle: Padding(
+        padding: const EdgeInsets.only(top: 8),
+        child: Text(context.t.settings.postLimit.desc),
+      ),
+      trailing: DropdownButton(
+        menuMaxHeight: 178,
+        value:
+            ref.watch(serverSettingStateProvider.select((it) => it.postLimit)),
+        elevation: 1,
+        underline: const SizedBox.shrink(),
+        borderRadius: const BorderRadius.all(Radius.circular(5)),
+        items: List<DropdownMenuItem<int>>.generate(
+          10,
+          (i) {
+            final x = i * 10 + 10;
+            return DropdownMenuItem(
+              value: x,
+              child: Text('$x'),
+            );
+          },
+        ),
+        onChanged: (value) {
+          ref
+              .read(serverSettingStateProvider.notifier)
+              .setPostLimit(value as int);
+        },
+      ),
+    );
+  }
+}
+
+class _ClearCache extends ConsumerWidget {
+  const _ClearCache();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ListTile(
+      title: Text(context.t.settings.clearCache.title),
+      subtitle: Padding(
+        padding: const EdgeInsets.only(top: 8),
+        child: Text(context.t.settings.clearCache.desc),
+      ),
+      onTap: () async {
+        context.scaffoldMessenger.showSnackBar(SnackBar(
+          content: Text(context.t.clearing),
+          duration: const Duration(milliseconds: 500),
+        ));
+
+        await DefaultCacheManager().emptyCache();
+        await extended_image.clearDiskCachedImages();
+        extended_image.clearMemoryImageCache();
+
+        context.scaffoldMessenger.showSnackBar(
+          SnackBar(
+            content: Text(context.t.settings.clearCache.done),
+            duration: const Duration(milliseconds: 500),
+          ),
+        );
+      },
     );
   }
 }
