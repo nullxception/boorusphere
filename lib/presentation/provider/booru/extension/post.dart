e9c06e2a1b895fdee01f1269c9a7ee28cf79d7d2
Nauval Rizky <enuma.alrizky@gmail.com>
2022-12-04 20:51:02 +0700

    getHeaders: handle fallback when web post url is not declared

--- lib/presentation/provider/booru/extension/post.dart
+++ lib/presentation/provider/booru/extension/post.dart
@@ -1,18 +1,34 @@
 import 'package:boorusphere/data/provider.dart';
 import 'package:boorusphere/data/repository/booru/entity/post.dart';
+import 'package:boorusphere/data/repository/server/entity/server_data.dart';
+import 'package:boorusphere/presentation/provider/server_data_state.dart';
 import 'package:boorusphere/utils/extensions/string.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 extension PostExt on Post {
+  String _findReferer(WidgetRef ref) {
+    if (postUrl.isNotEmpty) {
+      return postUrl;
+    }
+    final server = ref
+        .read(serverDataStateProvider)
+        .getById(serverId, or: ServerData.empty);
+    if (server.homepage.isNotEmpty) {
+      return server.homepage;
+    } else {
+      return originalFile.toUri().origin;
+    }
+  }
+
   Future<Map<String, String>> getHeaders(WidgetRef ref) async {
-    final cookies = CookieManager.getCookies(
-      await ref.read(cookieJarProvider).loadForRequest(postUrl.toUri()),
-    );
+    final referer = _findReferer(ref);
+    final cookies =
+        await ref.read(cookieJarProvider).loadForRequest(referer.toUri());
 
     return {
-      'Referer': postUrl,
-      'Cookie': cookies,
+      'Referer': referer,
+      'Cookie': CookieManager.getCookies(cookies),
     };
   }
 }
