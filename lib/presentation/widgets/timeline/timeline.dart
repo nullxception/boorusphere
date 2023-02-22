47e5c7aa87947024fd15ff6f87ee81fbbea4d798
Nauval Rizky <enuma.alrizky@gmail.com>
2023-02-23 05:04:51 +0700

    reorganize timeline

--- lib/presentation/widgets/timeline/timeline.dart
+++ lib/presentation/widgets/timeline/timeline.dart
@@ -13,6 +13,7 @@ import 'package:boorusphere/presentation/utils/extensions/post.dart';
 import 'package:boorusphere/presentation/widgets/timeline/timeline_controller.dart';
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:scroll_to_index/scroll_to_index.dart';
@@ -27,17 +28,8 @@ class Timeline extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final grid = ref.watch(uiSettingStateProvider.select((ui) => ui.grid));
-    final blurExplicit =
-        ref.watch(contentSettingStateProvider.select((it) => it.blurExplicit));
-    final timelineController = ref.watch(timelineControllerProvider);
-
     final screenWidth = context.mediaQuery.size.width;
     final flexibleGrid = (screenWidth / 200).round() + grid;
-    final scrollController = timelineController.scrollController;
-
-    String buildHeroTag(Post post) {
-      return '${timelineController.hashCode}-${post.serverId}@${post.id}';
-    }
 
     return SliverMasonryGrid.count(
       crossAxisCount: flexibleGrid,
@@ -46,65 +38,86 @@ class Timeline extends ConsumerWidget {
       crossAxisSpacing: 5,
       childCount: posts.length,
       itemBuilder: (context, index) {
-        final post = posts.elementAt(index);
-        return AutoScrollTag(
-          key: ValueKey(index),
-          controller: scrollController,
-          index: index,
-          highlightColor: context.theme.colorScheme.surfaceTint,
-          child: Card(
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(5)),
-            ),
-            // saveLayer() is used here to avoid artifacts that frequently
-            // happened while scrolling
-            clipBehavior: Clip.antiAliasWithSaveLayer,
-            child: GestureDetector(
-              child: Hero(
-                tag: buildHeroTag(post),
-                child: _Thumbnail(
-                  post: post,
-                  blurExplicit: blurExplicit,
-                ),
-                flightShuttleBuilder: (flightContext, animation,
-                    flightDirection, fromHeroContext, toHeroContext) {
-                  final Hero toHero = toHeroContext.widget as Hero;
-                  return Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      AspectRatio(
-                        aspectRatio: post.aspectRatio,
-                        // clip incoming child to avoid overflow that might be
-                        // caused by blurExplicit enabled
-                        child: flightDirection == HeroFlightDirection.pop
-                            ? ClipRect(child: toHero.child)
-                            : toHero.child,
-                      ),
-                    ],
-                  );
-                },
-              ),
-              onTap: () {
-                context.scaffoldMessenger.removeCurrentSnackBar();
-                context.router.push(
-                  PostRoute(
-                    beginPage: index,
-                    posts: posts,
-                    heroTagBuilder: buildHeroTag,
-                    timelineController: timelineController,
-                  ),
-                );
-              },
-            ),
-          ),
-        );
+        return _ThumbnailCard(index: index, posts: posts);
       },
     );
   }
 }
 
-class _Thumbnail extends HookConsumerWidget {
-  const _Thumbnail({
+class _ThumbnailCard extends HookConsumerWidget {
+  const _ThumbnailCard({
+    required this.index,
+    required this.posts,
+  });
+
+  final int index;
+  final Iterable<Post> posts;
+
+  String buildHeroTag(post) {
+    return '${posts.hashCode}@${post.serverId}@${post.id}';
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final post = useMemoized(() => posts.elementAt(index));
+    final scrollController = ref
+        .watch(timelineControllerProvider.select((it) => it.scrollController));
+    final blurExplicit =
+        ref.watch(contentSettingStateProvider.select((it) => it.blurExplicit));
+
+    return AutoScrollTag(
+      key: ValueKey(index),
+      controller: scrollController,
+      index: index,
+      highlightColor: context.theme.colorScheme.surfaceTint,
+      child: Card(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+        ),
+        // saveLayer() is used here to avoid artifacts that frequently
+        // happened while scrolling
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        child: GestureDetector(
+          child: Hero(
+            tag: buildHeroTag(post),
+            flightShuttleBuilder: (flightContext, animation, flightDirection,
+                fromHeroContext, toHeroContext) {
+              final Hero toHero = toHeroContext.widget as Hero;
+              return Stack(
+                alignment: Alignment.center,
+                children: [
+                  AspectRatio(
+                    aspectRatio: post.aspectRatio,
+                    // clip incoming child to avoid overflow that might be
+                    // caused by blurExplicit enabled
+                    child: flightDirection == HeroFlightDirection.pop
+                        ? ClipRect(child: toHero.child)
+                        : toHero.child,
+                  ),
+                ],
+              );
+            },
+            child: _ThumbnailImage(post: post, blurExplicit: blurExplicit),
+          ),
+          onTap: () {
+            context.scaffoldMessenger.removeCurrentSnackBar();
+            context.router.push(
+              PostRoute(
+                beginPage: index,
+                posts: posts,
+                heroTagBuilder: buildHeroTag,
+                timelineController: ref.read(timelineControllerProvider),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class _ThumbnailImage extends HookConsumerWidget {
+  const _ThumbnailImage({
     required this.post,
     this.blurExplicit = false,
   });
@@ -119,81 +132,87 @@ class _Thumbnail extends HookConsumerWidget {
     final isLong = post.aspectRatio < 0.5;
 
     final image = AspectRatio(
-        aspectRatio: isLong ? 0.5 : post.aspectRatio,
-        child: ExtendedImage.network(
-          // load sample photo when it's above 35:9
-          post.aspectRatio < 0.26 && post.sampleFile.asContent().isPhoto
-              ? post.sampleFile
-              : post.previewFile,
-          headers: headers.data,
-          fit: BoxFit.cover,
-          enableLoadState: false,
-          beforePaintImage: (canvas, rect, image, paint) {
-            if (blurExplicit && post.rating.isExplicit) {
-              paint.imageFilter = ImageFilter.blur(
-                sigmaX: 5,
-                sigmaY: 5,
-                tileMode: TileMode.decal,
-              );
-            }
-            return false;
-          },
-          loadStateChanged: (state) {
-            if (state.wasSynchronouslyLoaded && state.isCompleted) {
-              return state.completedWidget;
-            }
-
-            return AnimatedSwitcher(
-              duration: const Duration(milliseconds: 150),
-              child: state.isCompleted
-                  ? state.completedWidget
-                  : _Placeholder(
-                      key: ValueKey(post.id),
-                      isFailed: state.isFailed,
-                    ),
-              layoutBuilder: (currentChild, previousChildren) {
-                return Stack(
-                  alignment: Alignment.center,
-                  fit: StackFit.passthrough,
-                  children: [
-                    ...previousChildren,
-                    if (currentChild != null) currentChild,
-                  ],
-                );
-              },
+      aspectRatio: isLong ? 0.5 : post.aspectRatio,
+      child: ExtendedImage.network(
+        // load sample photo when it's above 35:9
+        post.aspectRatio < 0.26 && post.sampleFile.asContent().isPhoto
+            ? post.sampleFile
+            : post.previewFile,
+        headers: headers.data,
+        fit: BoxFit.cover,
+        enableLoadState: false,
+        beforePaintImage: (canvas, rect, image, paint) {
+          if (blurExplicit && post.rating.isExplicit) {
+            paint.imageFilter = ImageFilter.blur(
+              sigmaX: 5,
+              sigmaY: 5,
+              tileMode: TileMode.decal,
             );
-          },
-        ));
+          }
+          return false;
+        },
+        loadStateChanged: (state) {
+          if (state.wasSynchronouslyLoaded && state.isCompleted) {
+            return state.completedWidget;
+          }
+
+          return AnimatedSwitcher(
+            duration: const Duration(milliseconds: 150),
+            child: state.isCompleted
+                ? state.completedWidget
+                : _Placeholder(isFailed: state.isFailed),
+            layoutBuilder: (currentChild, previousChildren) {
+              return Stack(
+                alignment: Alignment.center,
+                fit: StackFit.passthrough,
+                children: [
+                  ...previousChildren,
+                  if (currentChild != null) currentChild,
+                ],
+              );
+            },
+          );
+        },
+      ),
+    );
 
     return isLong
         ? Stack(
             alignment: Alignment.bottomCenter,
             children: [
               image,
-              Card(
-                color: context.colorScheme.background.withOpacity(0.8),
-                elevation: 0,
-                margin: EdgeInsets.zero,
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(10),
-                  ),
-                ),
-                child: const Padding(
-                  padding: EdgeInsets.fromLTRB(22, 6, 22, 4),
-                  child: Icon(Icons.gradient, size: 16),
-                ),
-              )
+              const _LongThumbnailIndicator(),
             ],
           )
         : image;
   }
 }
 
+class _LongThumbnailIndicator extends StatelessWidget {
+  const _LongThumbnailIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: context.colorScheme.background.withOpacity(0.8),
+      elevation: 0,
+      margin: EdgeInsets.zero,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(10),
+          topRight: Radius.circular(10),
+        ),
+      ),
+      child: const Padding(
+        padding: EdgeInsets.fromLTRB(22, 6, 22, 4),
+        child: Icon(Icons.gradient, size: 16),
+      ),
+    );
+  }
+}
+
 class _Placeholder extends StatelessWidget {
   const _Placeholder({
-    super.key,
     this.isFailed = false,
   });
 
@@ -214,22 +233,17 @@ class _Placeholder extends StatelessWidget {
 
     return Shimmer(
       gradient: LinearGradient(
-          begin: Alignment.centerLeft,
-          end: Alignment.centerRight,
-          colors: <Color>[
-            baseColor,
-            baseColor,
-            highlightColor,
-            baseColor,
-            baseColor
-          ],
-          stops: const <double>[
-            0.0,
-            0.35,
-            0.5,
-            0.65,
-            1.0
-          ]),
+        begin: Alignment.centerLeft,
+        end: Alignment.centerRight,
+        colors: <Color>[
+          baseColor,
+          baseColor,
+          highlightColor,
+          baseColor,
+          baseColor
+        ],
+        stops: const <double>[0.0, 0.35, 0.5, 0.65, 1.0],
+      ),
       period: const Duration(milliseconds: 700),
       child: Container(
         color: Colors.black,
