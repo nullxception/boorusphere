a95803fc54a11f07d036e5e98df7086c628c0de5
Nauval Rizky <enuma.alrizky@gmail.com>
2023-03-21 12:56:46 +0700

    usePrecachePosts: support for range-of-post

--- lib/presentation/screens/post/hooks/precache_posts.dart
+++ lib/presentation/screens/post/hooks/precache_posts.dart
@@ -1,6 +1,7 @@
 import 'package:boorusphere/data/repository/booru/entity/post.dart';
 import 'package:boorusphere/presentation/provider/booru/post_headers_factory.dart';
 import 'package:boorusphere/presentation/utils/extensions/post.dart';
+import 'package:collection/collection.dart';
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -8,18 +9,20 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void Function(int index, bool loadOriginal) usePrecachePosts(
   WidgetRef ref,
-  Iterable<Post> posts,
-) {
-  return use(_PrecachePostsHook(ref, posts));
+  Iterable<Post> posts, {
+  int range = 2,
+}) {
+  return use(_PrecachePostsHook(ref, posts, range));
 }
 
 typedef _Precacher = void Function(int, bool);
 
 class _PrecachePostsHook extends Hook<_Precacher> {
-  const _PrecachePostsHook(this.ref, this.posts);
+  const _PrecachePostsHook(this.ref, this.posts, this.range);
 
   final WidgetRef ref;
   final Iterable<Post> posts;
+  final int range;
 
   @override
   HookState<_Precacher, _PrecachePostsHook> createState() {
@@ -46,19 +49,22 @@ class _PrecachePostsState extends HookState<_Precacher, _PrecachePostsHook> {
     }
   }
 
+  Iterable<Post> _postsInRange(int curr, int range) {
+    final prev = hook.posts.whereIndexed((index, element) {
+      return index < curr && index >= curr - range;
+    });
+
+    final next = hook.posts.whereIndexed((index, element) {
+      return index > curr && index <= curr + range;
+    });
+
+    return {...prev, ...next};
+  }
+
   void _precachePosts(i, showOG) {
     if (!context.mounted) return;
-
-    final next = i + 1;
-    final prev = i - 1;
-    final posts = hook.posts;
-
-    if (prev >= 0) {
-      _precacheImagePost(posts.elementAt(prev), showOG);
-    }
-
-    if (next < posts.length) {
-      _precacheImagePost(posts.elementAt(next), showOG);
+    for (var post in _postsInRange(i, hook.range)) {
+      _precacheImagePost(post, showOG);
     }
   }
 
