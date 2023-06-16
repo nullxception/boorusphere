b648e1148e7f71d0c2f308ff4650f4054c2de396
Nauval Rizky <enuma.alrizky@gmail.com>
2023-06-16 18:39:20 +0700

    test Shimmie parser

---
+++ test/repo/booru/shimmie_test.dart
@@ -0,0 +1,79 @@
+import 'package:boorusphere/data/provider.dart';
+import 'package:boorusphere/data/repository/booru/entity/page_option.dart';
+import 'package:boorusphere/data/repository/booru/parser/shimmie_xml_parser.dart';
+import 'package:boorusphere/data/repository/server/entity/server.dart';
+import 'package:boorusphere/data/repository/server/user_server_repo.dart';
+import 'package:boorusphere/domain/provider.dart';
+import 'package:boorusphere/main.dart';
+import 'package:boorusphere/presentation/provider/settings/entity/booru_rating.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../presentation/state/app_version_test.dart';
+import '../../utils/dio.dart';
+import '../../utils/fake_data.dart';
+import '../../utils/hive.dart';
+import '../../utils/mocktail.dart';
+import '../../utils/riverpod.dart';
+
+void main() async {
+  setupLogger();
+  setupMocktailFallbacks();
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  test('Shimmie', () async {
+    final ref = ProviderContainer(overrides: [
+      defaultServersProvider.overrideWithValue(await provideDefaultServers()),
+      envRepoProvider.overrideWithValue(FakeEnvRepo()),
+    ]);
+
+    initializeTestHive();
+    addTearDown(() async {
+      await destroyTestHive();
+      ref.dispose();
+    });
+
+    await UserServerRepo.prepare();
+    ref.setupTestFor(dioProvider);
+    final adapter = DioAdapterMock.on(ref.read(dioProvider));
+
+    ref.setupTestFor(serverRepoProvider);
+    await ref.read(serverRepoProvider).populate();
+    final parser = ShimmieXmlParser();
+    final server = Server(
+        homepage: 'https://rule34.paheal.net',
+        searchUrl: parser.searchQuery,
+        tagSuggestionUrl: parser.suggestionQuery);
+
+    const option = PageOption(limit: 5, searchRating: BooruRating.all);
+    final okHeadersXml = {
+      Headers.contentTypeHeader: ['text/xml']
+    };
+    final okHeadersJson = {
+      Headers.contentTypeHeader: [Headers.jsonContentType]
+    };
+
+    final fakePage = getFakeData('rule34.paheal/posts.xml').readAsStringSync();
+    when(() => adapter.fetch(any(), any(), any())).thenAnswer((_) async =>
+        ResponseBody.fromString(fakePage, 200, headers: okHeadersXml));
+
+    expect(
+      await ref.read(imageboardRepoProvider(server)).getPage(option, 1),
+      isA<Iterable>().having((x) => x.length, 'total', option.limit - 2),
+      reason: 'expecting 2 invalid post',
+    );
+
+    final fakeTags = getFakeData('rule34.paheal/tags.json').readAsStringSync();
+    when(() => adapter.fetch(any(), any(), any())).thenAnswer((_) async =>
+        ResponseBody.fromString(fakeTags, 200, headers: okHeadersJson));
+
+    expect(
+      await ref.read(imageboardRepoProvider(server)).getSuggestion('book'),
+      isA<Iterable>()
+          .having((x) => x.length, 'total', Server.tagSuggestionLimit - 2),
+      reason: 'expecting 2 tags with zero post_count',
+    );
+  });
+}
