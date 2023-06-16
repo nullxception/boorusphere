0940d0ad63245871495b3a8cdf143c4ed11869bd
Nauval Rizky <enuma.alrizky@gmail.com>
2023-06-16 19:07:53 +0700

    test szurubooru parser

---
+++ test/repo/booru/szurubooru_test.dart
@@ -0,0 +1,80 @@
+import 'package:boorusphere/data/provider.dart';
+import 'package:boorusphere/data/repository/booru/entity/page_option.dart';
+import 'package:boorusphere/data/repository/booru/parser/szurubooru_json_parser.dart';
+import 'package:boorusphere/data/repository/server/entity/server.dart';
+import 'package:boorusphere/data/repository/server/user_server_repo.dart';
+import 'package:boorusphere/domain/provider.dart';
+import 'package:boorusphere/main.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../presentation/state/app_version_test.dart';
+import '../../utils/dio.dart';
+import '../../utils/fake_data.dart';
+import '../../utils/hive.dart';
+import '../../utils/mocktail.dart';
+import '../../utils/riverpod.dart';
+
+void main() async {
+  setupLogger();
+  setupMocktailFallbacks();
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  test('Szurubooru', () async {
+    final ref = ProviderContainer(overrides: [
+      defaultServersProvider.overrideWithValue(await provideDefaultServers()),
+      envRepoProvider.overrideWithValue(FakeEnvRepo()),
+    ]);
+
+    initializeTestHive();
+    addTearDown(() async {
+      await destroyTestHive();
+      ref.dispose();
+    });
+
+    await UserServerRepo.prepare();
+    ref.setupTestFor(dioProvider);
+    final adapter = DioAdapterMock.on(ref.read(dioProvider));
+
+    final parser = SzurubooruJsonParser();
+    final server = Server(
+        homepage: 'https://homestuck.net/resources/booru',
+        searchUrl: parser.searchQuery,
+        tagSuggestionUrl: parser.suggestionQuery);
+
+    const option = PageOption(limit: 5);
+    final jsonHeaders = {
+      Headers.contentTypeHeader: [Headers.jsonContentType]
+    };
+
+    final fakePage = getFakeData('szurubooru/posts.json').readAsStringSync();
+    when(() => adapter.fetch(any(), any(), any())).thenAnswer((_) async =>
+        ResponseBody.fromString(fakePage, 200, headers: jsonHeaders));
+
+    expect(
+      await ref.read(imageboardRepoProvider(server)).getPage(option, 1),
+      isA<Iterable>().having((x) => x.length, 'total', option.limit - 2),
+      reason: 'expecting 2 invalid post',
+    );
+
+    final fakeTags = getFakeData('szurubooru/tags.json').readAsStringSync();
+    when(() => adapter.fetch(any(), any(), any())).thenAnswer((_) async =>
+        ResponseBody.fromString(fakeTags, 403, headers: jsonHeaders));
+
+    await expectLater(
+      ref.read(imageboardRepoProvider(server)).getSuggestion('book'),
+      throwsA(isA<DioException>()),
+      reason: 'expecting error because not logged in',
+    );
+
+    when(() => adapter.fetch(any(), any(), any())).thenAnswer((_) async =>
+        ResponseBody.fromString(fakeTags, 200, headers: jsonHeaders));
+    expect(
+      await ref.read(imageboardRepoProvider(server)).getSuggestion('book'),
+      isA<Iterable>().having((x) => x.length, 'total', 4 - 1),
+      reason: 'expecting 4 result with 1 tags having zero post_count',
+    );
+  });
+}
