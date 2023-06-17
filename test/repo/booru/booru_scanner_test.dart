2b5df7286b1f05384868d31bb1957d12479e5ccf
Nauval Rizky <enuma.alrizky@gmail.com>
2023-06-18 04:32:53 +0700

    test: BooruScanner: add Safebooru test

--- test/repo/booru/booru_scanner_test.dart
+++ test/repo/booru/booru_scanner_test.dart
@@ -4,6 +4,7 @@ import 'package:boorusphere/data/repository/booru/parser/danbooru_json_parser.da
 import 'package:boorusphere/data/repository/booru/parser/gelbooru_json_parser.dart';
 import 'package:boorusphere/data/repository/booru/parser/gelbooru_xml_parser.dart';
 import 'package:boorusphere/data/repository/booru/parser/moebooru_json_parser.dart';
+import 'package:boorusphere/data/repository/booru/parser/safebooru_xml_parser.dart';
 import 'package:boorusphere/data/repository/booru/provider.dart';
 import 'package:boorusphere/data/repository/booru/utils/booru_scanner.dart';
 import 'package:boorusphere/domain/provider.dart';
@@ -122,6 +123,36 @@ void main() async {
       expect(result.searchParserId, parser.id);
       expect(result.tagSuggestionUrl, parser.suggestionQuery);
       expect(result.suggestionParserId, parser.id);
+      expect(result.postUrl, parser.postUrl);
+    });
+
+    test('Safebooru', () async {
+      const host = 'https://safebooru.test';
+      adapter
+        ..put(
+          '$host/index.php?page=dapi&s=post&q=index&tags=*&pid=1&limit=3',
+          FakeResponseBody.fromFakeData('safebooru/posts.xml', 200),
+        )
+        ..put(
+          '$host/index.php?page=dapi&s=tag&q=index&name_pattern=%25a%25&orderby=count&limit=3',
+          FakeResponseBody.fromFakeData('safebooru/tags.xml', 200),
+        )
+        ..put(
+          '$host/index.php?page=post&s=view&id=100',
+          FakeResponseBody.fromFakeData('safebooru/post.html', 200),
+        );
+
+      final scanner =
+          BooruScanner(parsers: parsers, client: ref.read(dioProvider));
+
+      final result = await scanner.scan(host, host);
+      final parser = GelbooruXmlParser();
+      final resultParser = SafebooruXmlParser();
+
+      expect(result.searchUrl, parser.searchQuery);
+      expect(result.searchParserId, resultParser.id);
+      expect(result.tagSuggestionUrl, parser.suggestionQuery);
+      expect(result.suggestionParserId, resultParser.id);
       expect(result.postUrl, parser.postUrl);
     });
   });
