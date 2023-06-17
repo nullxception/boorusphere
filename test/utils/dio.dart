e70000f9e254eabe29ecee9bcbd9746139e296cf
Nauval Rizky <enuma.alrizky@gmail.com>
2023-06-17 20:35:53 +0700

    test: add FakeResponseInterceptor

--- test/utils/dio.dart
+++ test/utils/dio.dart
@@ -7,8 +7,21 @@ import 'package:mocktail/mocktail.dart';
 import 'fake_data.dart';
 
 class DioAdapterMock extends Mock implements HttpClientAdapter {
-  DioAdapterMock(Dio dio) {
-    dio.httpClientAdapter = this;
+  DioAdapterMock(this.client, {this.byIntercepting = false}) {
+    client.httpClientAdapter = this;
+    if (byIntercepting) {
+      client.interceptors.add(FakeResponseInterceptor(holder));
+    }
+  }
+
+  final Dio client;
+  final bool byIntercepting;
+
+  late ResponseHolder holder = ResponseHolder(client);
+
+  void put(String url, ResponseBody body) {
+    assert(byIntercepting);
+    holder.put(url, body);
   }
 }
 
@@ -26,5 +39,42 @@ class FakeResponseBody {
         ...?headers,
       },
     );
+  }
+}
+
+class FakeResponseInterceptor extends Interceptor {
+  const FakeResponseInterceptor(this.holder);
+
+  final ResponseHolder holder;
+
+  @override
+  Future<void> onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
+    handler.resolve(await holder.responseFor(options));
+  }
+}
+
+class ResponseHolder {
+  ResponseHolder(this.client);
+
+  final Dio client;
+
+  final Map<Uri, ResponseBody> responses = {};
+
+  Future<Response> responseFor(RequestOptions options) async {
+    final notFound = ResponseBody.fromBytes([], 404);
+    final body = responses[options.uri] ?? notFound;
+
+    return Response(
+      data: await client.transformer.transformResponse(options, body),
+      requestOptions: options,
+      statusCode: body.statusCode,
+    );
+  }
+
+  void put(String url, ResponseBody responseBody) {
+    responses[url.toUri()] = responseBody;
   }
 }
