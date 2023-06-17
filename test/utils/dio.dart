474eabb63e483cb03024f3f924fec4e83171af0b
Nauval Rizky <enuma.alrizky@gmail.com>
2023-06-17 21:24:30 +0700

    FakeResponseInterceptor: add support for validating status

--- test/utils/dio.dart
+++ test/utils/dio.dart
@@ -52,7 +52,17 @@ class FakeResponseInterceptor extends Interceptor {
     RequestOptions options,
     RequestInterceptorHandler handler,
   ) async {
-    handler.resolve(await holder.responseFor(options));
+    final response = await holder.responseFor(options);
+    if (!options.validateStatus(response.statusCode)) {
+      handler.reject(DioException.badResponse(
+        statusCode: response.statusCode ?? 200,
+        requestOptions: options,
+        response: response,
+      ));
+      return;
+    }
+
+    handler.resolve(response);
   }
 }
 
@@ -62,10 +72,30 @@ class ResponseHolder {
   final Dio client;
 
   final Map<Uri, ResponseBody> responses = {};
+  Response _notFoundResponse(RequestOptions options) {
+    return Response(
+      data: ResponseBody.fromBytes([], 404, isRedirect: true),
+      requestOptions: options,
+      isRedirect: true,
+      redirects: [
+        RedirectRecord(
+          301,
+          'GET',
+          options.uri.replace(
+            queryParameters: {},
+            path: '/notfound',
+            fragment: '',
+          ),
+        )
+      ],
+    );
+  }
 
   Future<Response> responseFor(RequestOptions options) async {
-    final notFound = ResponseBody.fromBytes([], 404);
-    final body = responses[options.uri] ?? notFound;
+    final body = responses[options.uri];
+    if (body == null) {
+      return _notFoundResponse(options);
+    }
 
     return Response(
       data: await client.transformer.transformResponse(options, body),
